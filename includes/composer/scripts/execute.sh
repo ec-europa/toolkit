286d0a5e405c1cf3810d1a62b0dfad83342cdc80
Alex Verbruggen <verbruggenalex@gmail.com>
2017-07-20 07:40:57 -0700

    Execute phingcalls through bash for composer hooks

--- includes/composer/scripts/execute.sh
+++ includes/composer/scripts/execute.sh
@@ -1,11 +1,26 @@
-#!/bin/sh
-
-# Post install or update.
+#!/bin/bash
 
 PHING=$(pwd)/bin/phing
-if [ -f $PHING ] && [ -x $PHING ] ; then
-  echo "Executing $1 ping targets."
-  $PHING composer-$1-cmd -Dproject.basedir=$PROJECT -logger phing.listener.AnsiColorLogger -find >/dev/null
+
+# If phing is available.
+if [ -f $PHING ] && [ -x $PHING ] && [ -n "$1" ] ; then
+
+  # Get phing targets.
+  HOOK="composer.hook.${1//-/.}"
+  PHINGCALLS=$(\
+    $PHING composer-get-hook-phingcalls \
+    -Dproject.basedir=$PROJECT \
+    -logger phing.listener.AnsiColorLogger \
+    -find | grep -o "$HOOK=.*" | cut -f2- -d=\
+  )
+
+  # Execute targets, if any.
+  if [ -n "$PHINGCALLS" ] ; then
+    $PHING $PHINGCALLS -Dproject.basedir=$PROJECT -logger phing.listener.AnsiColorLogger -find
+  fi
+
 else
-  echo "Phing unavailable."
+  # Notify user that no phing targets can be executed for the hook.
+  echo "Phing unavailable:"
+  echo "No composer hooks will be executed for $1."
 fi
