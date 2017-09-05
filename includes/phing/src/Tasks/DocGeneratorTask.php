1465c38bece04114dc49c7927741d9dda28c4c7b
Alex Verbruggen <verbruggenalex@gmail.com>
2017-09-05 00:12:39 -0700

    Refractor for build props

--- includes/phing/src/Tasks/DocGeneratorTask.php
+++ includes/phing/src/Tasks/DocGeneratorTask.php
@@ -13,6 +13,43 @@ require_once 'phing/Task.php';
 class DocGeneratorTask extends \Task {
 
   /**
+   * The location of the build file to generate docs for.
+   *
+   * @var string
+   */
+  private $buildfile = '';
+
+  private function getBuildList($buildFile, &$buildList = array()) {
+
+    if ($xml = simplexml_load_file($buildFile)) {
+      $importFiles = array();
+
+      foreach ($xml->xpath('//import[@file]') as $import) {
+        $importFile = (string)$import->attributes()->file;
+        // Replace tokens.
+        if (preg_match_all('/\$\{(.*?)\}/s', $importFile, $matches)) {
+          foreach ($matches[0] as $key => $match) {
+            $tokenText = $this->getProject()->getProperty($matches[1][$key]);
+            $importFile = str_replace($match, $tokenText, $importFile);
+           }
+        }
+        $importFiles[] = $importFile;
+      }
+      array_walk_recursive($buildList,
+        function (&$v, $k, $u) {
+          if ($v == $u[0]) {
+            $v = array($u[0] => $u[1]);
+          }
+        },
+        [$buildFile, $importFiles]);
+
+      foreach($importFiles as $importFile) {
+        $this->getBuildList($importFile, $buildList);
+      }
+    }
+  }
+
+  /**
    * Generates an aliases.drushrc.php file.
    *
    * Either generates a file for:
@@ -21,12 +58,13 @@ class DocGeneratorTask extends \Task {
    */
   public function main() {
 
-    ini_set('xdebug.var_display_max_depth', 5);
+    ini_set('xdebug.var_display_max_depth', 10);
 
     $project = $this->getProject();
     $basedir = $project->getBasedir();
-    $targets = $project->getTargets();
-    var_dump(count($targets));
+    $buildFile = $this->buildFile;
+    $buildList = array($buildFile);
+    $this->getBuildList($buildFile, $buildList);
 
     $buildFiles = new Finder();
     $buildFiles
@@ -38,31 +76,40 @@ class DocGeneratorTask extends \Task {
     $wrapperTargets = array();
     $playbookTargets = array();
     $callbackTargets = array();
+    $deprecatedTargets = array();
     $helperTargets = array();
 
     foreach ($buildFiles as $buildFile) {
-      $xml = simplexml_load_file($buildFile->getRealPath());
-      $buildFilePath = $buildFile->getRelativePathname();
+      $buildFilePath = $buildFile->getRealPath();
+      $xml = simplexml_load_file($buildFilePath);
 
       foreach ($xml->xpath('//target') as $target) {
 
         $targetName = (string)$target->attributes()->name;
+        $targetVisibility = (string)$target->attributes()->hidden == 'true' ? 'hidden' : 'visible';
         $targetDescription = (string)$target->attributes()->description;
 
-        $targetsArray[$targetName] = array(
+        $targetArray = array(
+          'name' => $targetName,
           'description' => $targetDescription,
+          'visibility' => $targetVisibility,
           'buildfile' => $buildFilePath,
         );
 
-        if (isset($target->attributes()->depends) && substr($targetName, -9) === '-playbook') {
+        if (isset($target->attributes()->depends)) {
           $targetDependenciesString = (string)$target->xpath('./@depends')[0];
           $targetDependencies = explode(',', str_replace(' ', '', $targetDependenciesString));
           $callbackTargets = array_merge($callbackTargets, $targetDependencies);
-          $targetsArray[$targetName] += array(
-            'type' => 'playbook',
+          $targetArray += array(
             'dependencies' => $targetDependencies,
           );
-          $playbookTargets[$targetName] = $targetsArray[$targetName];
+        }
+
+        if (substr($targetName, -9) === '-playbook') {
+          $targetArray += array(
+            'type' => 'playbook',
+          );
+          $playbookTargets[] = $targetName;
         }
 
         if (count($target->xpath('./phingcall')) == 1 && count($target->xpath('./phingcall[1]/property')) > 0) {
@@ -78,49 +125,102 @@ class DocGeneratorTask extends \Task {
                 'description' => 'Description',
               );
             }
-            $targetsArray[$targetName] += array(
+            $targetArray += array(
+              'type' => 'wrapper',
               'properties' => $props,
               'phingcall' => $phingCallTarget,
             );
-            $wrapperTargets[$targetName] = $targetsArray[$targetName];
+            $wrapperTargets = $targetName;
           }
         }
 
+        if (count($target->xpath('./replacedby')) == 1) {
+          $replacedBy = (string)$target->xpath('./replacedby[1]/@target')[0];
+          $deprecatedTargets[] = $targetName;
+          $targetArray = array_merge($targetArray, array(
+            'type' => 'deprecated',
+            'description' => $replacedBy,
+          ));
+        }
+
         $callbackTargets = array_unique($callbackTargets);
+        $targetsArray[] = $targetArray;
 
       }
     }
 
-    foreach ($buildFiles as $buildFile) {
-      $xml = simplexml_load_file($buildFile->getRealPath());
-      $buildFilePath = $buildFile->getRelativePathname();
-
-      foreach ($xml->xpath('//target') as $target) {
-
-        $targetName = (string)$target->attributes()->name;
-        $targetDescription = (string)$target->attributes()->description;
-
-        $targetsArray[$targetName] = array(
-          'description' => $targetDescription,
-          'buildfile' => $buildFilePath,
-        );
-
-        if (in_array($targetName, $callbackTargets)) {
-          $targetsArray[$targetName] += array(
-            'type' => 'callback',
-          );
-          $callbackTargets[$targetName] = $targetsArray[$targetName];
-        }
-        else {
-          $targetsArray[$targetName] += array(
-            'type' => 'helper',
-          );
-          $helperTargets[$targetName] = $targetsArray[$targetName];
-        }
+    foreach ($targetsArray as $key => $targetArray) {
+      if (in_array($targetArray['name'], $callbackTargets)) {
+        $targetsArray[$key]['type'] = 'callback';
+      }
+      elseif (!isset($targetArray['type'])) {
+        $targetsArray[$key]['type'] = 'helper';
       }
     }
     $this->wrapperTargetTable($wrapperTargets, $playbookTargets, $callbackTargets);
-//   var_dump($callbackTargets);
+
+    $iterator = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($buildList), \RecursiveIteratorIterator::SELF_FIRST);
+    $output = '';
+    for($iterator; $iterator->valid(); $iterator->next())
+    {
+      $depth = ($iterator->getDepth() + 1) / 2;
+      if (is_file($iterator->key())) {
+        $buildFile = $iterator->key();
+        $xml = simplexml_load_file($buildFile);
+        $targets = array_filter($targetsArray, function($v, $k) use ($buildFile) {
+          return $v['buildfile'] === $buildFile;
+        }, ARRAY_FILTER_USE_BOTH);
+        $projectName = ucfirst((string) $xml->xpath('//project/@name')[0]);
+        $output .= str_repeat('#', $depth) . ' ' . $projectName . "\n";
+        if (!empty($projectName) && count($targets) > 1){
+          $output .= "<table>\n";
+          $output .= "    <thead>\n";
+          $output .= "        <tr align=\"left\">\n";
+          $output .= "            <th nowrap>Target type</th>\n";
+          $output .= "            <th nowrap>Name</th>\n";
+          $output .= "            <th nowrap>Description</th>\n";
+          $output .= "        </tr>\n";
+          $output .= "    </thead>\n";
+          $output .= "    <tbody>\n";
+          foreach ($targets as $targetName => $target) {
+            $output .= "        <tr>\n";
+            $output .= "            <td nowrap>\n";
+            if ($target['visibility'] === 'visible') {
+              $output .= "                <img src=\"https://cdn0.iconfinder.com/data/icons/octicons/1024/eye-16.png\" align=\"left\" alt=\"visible\" />\n";
+            }
+            else {
+              $output .= "                <img src=\"https://cdn0.iconfinder.com/data/icons/octicons/1024/gist-secret-20.png\" align=\"left\" alt=\"hidden\" />\n";
+            }
+            switch ($target['type']) {
+              case 'wrapper':
+                $output .= "                <img src=\"https://cdn0.iconfinder.com/data/icons/octicons/1024/star-20.png\" align=\"left\" alt=\"wrapper\" />\n";
+                break;
+              case 'playbook':
+                $output .= "                <img src=\"https://cdn0.iconfinder.com/data/icons/octicons/1024/three-bars-20.png\" align=\"left\" alt=\"playbook\" />\n";
+                break;
+              case 'deprecated':
+                $output .= "                <img src=\"https://cdn0.iconfinder.com/data/icons/octicons/1024/trashcan-20.png\" align=\"left\" alt=\"deprecated\" />\n";
+                break;
+              case 'helper':
+                $output .= "                <img src=\"https://cdn0.iconfinder.com/data/icons/octicons/1024/tools-16.png\" align=\"left\" alt=\"helper\" />\n";
+                break;
+              case 'callback':
+                $output .= "                <img src=\"https://cdn0.iconfinder.com/data/icons/octicons/1024/zap-20.png\" align=\"left\" alt=\"callback\" />\n";
+                break;
+            }
+            $output .= "            </td>\n";
+            $output .= "            <td nowrap>" . $target['name'] . "</td>\n";
+            $output .= "            <td width=\"80%\">" . $target['description'] . "</td>\n";
+            $output .= "        </tr>\n";
+          }
+          $output .= "    </tbody>\n";
+          $output .= "</table>\n\n";
+
+        }
+      }
+    }
+    echo "$output";
+//   var_dump($targetsArray);
   }
   
   protected function wrapperTargetTable($wrapperTargets, $playbookTargets, $callbackTargets) {
@@ -204,12 +304,12 @@ class DocGeneratorTask extends \Task {
     }
   }
   /**
-   * Sets the Phing directory to generate docs for.
+   * Sets the Phing file to generate docs for.
    *
-   * @param string $phingdir
+   * @param string $buildfile
    *   The Phing directory to generate docs for.
    */
-  public function setPhingdir($phingdir) {
-    $this->phingDir = $phingdir;
+  public function setBuildFile($buildfile) {
+    $this->buildFile = $buildfile;
   }
 }
