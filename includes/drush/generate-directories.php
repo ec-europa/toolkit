0147e07591b0400c275e7ba269cff3f095a88017
Alex Verbruggen <verbruggenalex@gmail.com>
2017-09-10 10:49:03 -0700

    Try fix

--- includes/drush/generate-directories.php
+++ includes/drush/generate-directories.php
@@ -21,20 +21,25 @@ $directories = array(
 
 foreach ($directories as $directory) {
   if (!is_dir($directory) && !@drupal_mkdir($directory, NULL, TRUE)) {
+    drush_log("The directory " . $directory . " does not exist and could not be created.");
     watchdog('file system', 'The directory %directory does not exist and could not be created.', array('%directory' => $directory), WATCHDOG_ERROR);
   }
   if (is_dir($directory) && !is_writable($directory) && !@drupal_chmod($directory)) {
+    drush_log("The directory " . $directory . " exists but is not writable and could not be made writable.");
     watchdog('file system', 'The directory %directory exists but is not writable and could not be made writable.', array('%directory' => $directory), WATCHDOG_ERROR);
   }
   elseif (is_dir($directory)) {
     $public_dir = variable_get('file_public_path', conf_path() . '/files');
-    if ($directory == $public_dir) {
-      // Create public .htaccess file.
-      file_create_htaccess($directory, FALSE);
+    $private = $directory == $public_dir ? FALSE : TRUE;
+    $htaccess_path =  $directory . '/.htaccess';
+    $htaccess_lines = file_htaccess_lines($private);
+    if (file_put_contents($htaccess_path, $htaccess_lines)) {
+      @drupal_chmod($htaccess_path, 0444);
     }
     else {
-      // Create private .htaccess file.
-      file_create_htaccess($directory);
+      drush_log("Security warning: Couldn't write .htaccess file. Please create a .htaccess file in your " . $directory . " directory.");
+      $variables = array('%directory' => $directory, '!htaccess' => '<br />' . nl2br(check_plain($htaccess_lines)));
+      watchdog('security', "Security warning: Couldn't write .htaccess file. Please create a .htaccess file in your %directory directory which contains the following lines: <code>!htaccess</code>", $variables, WATCHDOG_ERROR);
     }
   }
 }
