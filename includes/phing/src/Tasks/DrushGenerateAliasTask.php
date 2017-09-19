3abd2727764340f1bf14ff78dad849e5b4b3ba4a
Joao Santos <jonhy81@gmail.com>
2017-09-19 22:10:18 +0200

    Apply Pear coding style for Drush Generated Aliases Class.

--- includes/phing/src/Tasks/DrushGenerateAliasTask.php
+++ includes/phing/src/Tasks/DrushGenerateAliasTask.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Generate an aliases.drushrc.php file.
+ *
+ * PHP Version 5 and 7
+ *
+ * @category Documentation
+ * @package  SSK
+ * @author   DIGIT NEXTEUROPA QA <DIGIT-NEXTEUROPA-QA@ec.europa.eu>
+ * @license  https://ec.europa.eu/info/european-union-public-licence_en EUPL
+ * @link     https://github.com/ec-europa/ssk/blob/master/includes/phing/src/Tasks/DocGeneratorTask.php
+ */
+
 namespace Phing\Ssk\Tasks;
 
 use Project;
@@ -9,6 +21,12 @@ require_once 'phing/Task.php';
 
 /**
  * A Phing task to generate an aliases.drushrc.php file.
+ *
+ * @category Documentation
+ * @package  SSK
+ * @author   DIGIT NEXTEUROPA QA <DIGIT-NEXTEUROPA-QA@ec.europa.eu>
+ * @license  https://ec.europa.eu/info/european-union-public-licence_en EUPL
+ * @link     https://github.com/ec-europa/ssk/blob/master/includes/phing/src/Tasks/DocGeneratorTask.php
  */
 class DrushGenerateAliasTask extends \Task
 {
@@ -18,28 +36,28 @@ class DrushGenerateAliasTask extends \Task
      *
      * @var string
      */
-    private $aliasName = '';
+    private $_aliasName = '';
 
     /**
      * The uri of the alias to generate.
      *
      * @var string
      */
-    private $aliasUri = '';
+    private $_aliasUri = '';
 
     /**
      * The root directory of the website.
      *
      * @var string
      */
-    private $siteRoot = '';
+    private $_siteRoot = '';
 
     /**
      * The directory to save the aliases in.
      *
      * @var string
      */
-    private $drushDir = '/sites/all/drush';
+    private $_drushDir = '/sites/all/drush';
 
 
     /**
@@ -48,29 +66,31 @@ class DrushGenerateAliasTask extends \Task
      * Either generates a file for:
      *  - all sites in the sites directory.
      *  - a single site to be added to the aliases file (appending).
+     *
+     * @return void
      */
     public function main()
     {
         // Check if all required data is present.
         $this->checkRequirements();
 
-        $drushDir    = $this->drushDir == '/sites/all/drush' ? $this->siteRoot.$this->drushDir : $this->drushDir;
+        $drushDir    = $this->_drushDir == '/sites/all/drush' ? $this->_siteRoot.$this->_drushDir : $this->_drushDir;
         $aliasesFile = $drushDir.'/aliases.drushrc.php';
 
         $aliases = array(
                     'default' => array(
-                                  'uri'  => 'default',
-                                  'root' => $this->siteRoot,
-                                 ),
-                   );
+                        'uri'  => 'default',
+                        'root' => $this->_siteRoot,
+                      ),
+                    );
 
-        if (empty($this->aliasName)) {
+        if (empty($this->_aliasName)) {
             $sites = new Finder();
             $sites
                 ->directories()
                 ->depth('== 0')
                 ->exclude('all')
-                ->in($this->siteRoot.'/sites');
+                ->in($this->_siteRoot.'/sites');
 
             foreach ($sites as $site) {
                 $aliases[$site->getBasename()] = array(
@@ -78,11 +98,10 @@ class DrushGenerateAliasTask extends \Task
                                                   'root' => $aliases['default']['root'],
                                                  );
             }
-        }
-        else {
+        } else {
             $aliases += $this->loadAliases($aliasesFile);
-            $aliases[$this->aliasName] = array(
-                                          'uri'  => $this->aliasName,
+            $aliases[$this->_aliasName] = array(
+                                          'uri'  => $this->_aliasName,
                                           'root' => $aliases['default']['root'],
                                          );
         }//end if
@@ -91,32 +110,36 @@ class DrushGenerateAliasTask extends \Task
 
         if (file_put_contents($aliasesFile, $aliasesArray)) {
             $this->log("Succesfully wrote aliases to file '".$aliasesFile."'", Project::MSG_INFO);
-        }
-        else {
+        } else {
             $this->log("Was unable to write aliases to file '".$aliasesFile."'", Project::MSG_WARN);
         }
-
     }//end main()
-
 
     /**
      * Checks if all properties required for generating the aliases file are present.
      *
      * @throws \BuildException
      *   Thrown when a required property is not present.
+     *
+     * @return void
      */
     protected function checkRequirements()
     {
-        $required_properties = array('siteRoot');
+        $required_properties = array('_siteRoot');
         foreach ($required_properties as $required_property) {
             if (empty($this->$required_property)) {
                 throw new \BuildException("Missing required property '$required_property'.");
             }
         }
-
     }//end checkRequirements()
 
-
+    /**
+     * Load drush aliases.
+     *
+     * @param string $aliasesFile File with alias
+     *
+     * @return array|mixed
+     */
     protected function loadAliases($aliasesFile)
     {
         if (is_file($aliasesFile)) {
@@ -124,63 +147,57 @@ class DrushGenerateAliasTask extends \Task
         }
 
         return array();
-
     }//end loadAliases()
-
 
     /**
      * Sets the name of the alias to set.
      *
-     * @param string $aliasName
-     *   The name of the alias to set.
+     * @param string $aliasName The name of the alias to set.
+     *
+     * @return void
      */
     public function setAliasName($aliasName)
     {
-        $this->aliasName = $aliasName;
-
+        $this->_aliasName = $aliasName;
     }//end setAliasName()
-
 
     /**
      * Sets the uri of tha alias to set.
      *
-     * @param string $aliasUri
-     *   The uri of the alias to set.
+     * @param string $aliasUri The uri of the alias to set.
+     *
+     * @return void
      */
     public function setAliasUri($aliasUri)
     {
-        $this->aliasUri = $aliasUri;
-
+        $this->_aliasUri = $aliasUri;
     }//end setAliasUri()
-
 
     /**
      * Sets the root of the Drupal site.
      *
-     * @param string $siteRoot
-     *   The root of the Drupal site.
+     * @param string $siteRoot The root of the Drupal site.
+     *
+     * @return void
      */
     public function setSiteRoot($siteRoot)
     {
-        $this->siteRoot = $siteRoot;
-
+        $this->_siteRoot = $siteRoot;
     }//end setSiteRoot()
-
 
     /**
      * Sets the diurectory of drush to place the aliases in.
      *
-     * @param string $drushDir
-     *   The Drush directory to place the aliases in
+     * @param string $drushDir The Drush directory to place the aliases in
+     *
+     * @return void
      *
      * @todo: validate if it is a registered location of drush.
      * @link: https://github.com/drush-ops/drush/blob/master/examples/example.aliases.drushrc.php#L57
      */
     public function setDrushDir($drushDir)
     {
-        $this->drushDir = $drushDir;
-
+        $this->_drushDir = $drushDir;
     }//end setDrushDir()
-
 
 }//end class
