d347fe7483130c68a682863ba2725f240da58dd4
Alex Verbruggen <verbruggenalex@gmail.com>
2017-09-26 19:35:55 -0700

    Test hook

--- includes/git/hooks/phingcalls.sh
+++ includes/git/hooks/phingcalls.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PHING=$DIR/../../../bin/phing
-echo $(pwd)
+PHING="$DIR/../../bin/phing"
 
 # If phing is available.
 if [ -f $PHING ] && [ -x $PHING ] && [ -n "$1" ] ; then
