dc589bdcb95e75933bcfa32a7a6e015574ad8ea2
Alex Verbruggen <verbruggenalex@gmail.com>
2017-09-29 01:44:51 -0700

    Debug

--- includes/git/execute.sh
+++ includes/git/execute.sh
@@ -1,17 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 # Execute all scripts in the folder that matches the scripts filename.
 # This execution script is merely a template which will
 # be copied when there are git hooks defined.
-HOOK=$(basename $0)
+HOOK=$(basename $1)
 SOURCE="$0"
 SOURCEDIR=$( dirname "$SOURCE" )
+
 while [ -h "$SOURCE" ]; do
   DIR="$( cd -P $SOURCEDIR && pwd )"
   SOURCE="$(readlink "$SOURCE")"
   [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE"
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+echo $DIR
 
 HOOK_DIR="$DIR/hooks/$HOOK"
 if [ -d $HOOK_DIR ] ; then
