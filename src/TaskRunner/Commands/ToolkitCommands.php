61d06a168e6d4169b831170f144df9e8c2eb755c
João Santos <jonhy81@gmail.com>
2019-03-05 23:32:36 +0100

    MULTISITE-20951: Add new Toolkit commands class.

---
+++ src/TaskRunner/Commands/ToolkitCommands.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Eceuropa\Toolkit\TaskRunner\Commands;
+
+use OpenEuropa\TaskRunner\Commands\AbstractCommands;
+use Consolidation\AnnotatedCommand\CommandData;
+use NuvoleWeb\Robo\Task as NuvoleWebTasks;
+use OpenEuropa\TaskRunner\Contract\FilesystemAwareInterface;
+use OpenEuropa\TaskRunner\Tasks as TaskRunnerTasks;
+use OpenEuropa\TaskRunner\Traits as TaskRunnerTraits;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Yaml\Yaml;
+use GuzzleHttp\Client;
+
+/**
+ * Class ToolkitCommands
+ *
+ * @package Eceuropa\Toolkit\TaskRunner\Commands
+ */
+class ToolkitCommands extends AbstractCommands implements FilesystemAwareInterface
+{
+    const ASDA_URL = 'https://webgate.ec.europa.eu/fpfis/files-for/automate_dumps/';
+
+    public $dumpFilename = '';
+
+    use TaskRunnerTraits\ConfigurationTokensTrait;
+    use TaskRunnerTraits\FilesystemAwareTrait;
+    use TaskRunnerTasks\CollectionFactory\loadTasks;
+    use NuvoleWebTasks\Config\loadTasks;
+
+    /**
+     * Install clone from production snapshot.
+     *
+     * @command toolkit:clone
+     *
+     * @aliases tc
+     */
+    public function toolkitClone()
+    {
+      // Get updated dump if the case.
+      $this->toolkitDatabaseDownload();
+
+      // Dump database into location.
+      $databaseConfig = $this->config->get('database');
+
+      $this->taskExec('vendor/bin/drush --uri=web sqlc < .tmp/sanitized_daily_multisite_youth_production.sql')
+        ->run();
+    }
+
+    /**
+     * Download production snapshot.
+     *
+     * @command toolkit:database-download
+     *
+     * @aliases tdd
+     */
+    public function toolkitDatabaseDownload()
+    {
+
+      // Create folder if non-existent.
+      if (!is_dir('.tmp')) {
+        $this->taskExec('mkdir -p .tmp')->run();
+      }
+
+      $client = new Client();
+      $requestUrl = self::ASDA_URL . $this->config->get('project.id') . '/';
+      $requestOptions = [
+        'auth' => [
+          $this->config->get('asda.user'),
+          $this->config->get('asda.password'),
+        ]
+      ];
+
+      // Get current filename.
+      $response = $client->request('GET', $requestUrl, $requestOptions);
+      $body = (string) $response->getBody();
+
+      foreach(preg_split("/((\r?\n)|(\r\n?))/", trim(strip_tags($body))) as $key => $line){
+        $hashFilename = (strpos($line, '.sh')) ? trim($line) : 'latest.sh1';
+        $this->dumpFilename = (strpos($line, '.sql.gz')) ? trim($line) : '';
+      }
+
+      // Check if this files is already downloaded.
+      if (!is_file('.tmp/' . $this->dumpFilename)) {
+        // Download database.
+        if ($this->dumpFilename) {
+          $requestOptions += ['sink' => '.tmp/' . $this->dumpFilename];
+          $client->request('GET', $requestUrl . $this->dumpFilename, $requestOptions);
+        }
+      }
+    }
+
+    /**
+     * Run PHP code review
+     * @command toolkit:test-phpcs
+     *
+     * @aliases ttp
+     */
+    public function toolkitTestPhpcs()
+    {
+      return $this->taskExec("./vendor/bin/grumphp run")->run();
+    }
+
+}
