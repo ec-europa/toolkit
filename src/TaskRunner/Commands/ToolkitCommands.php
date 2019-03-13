1b0f58b0ee33c80d6e8997160ae79ac8b4ed6c89
Antonio De Marco <antonio@nuvole.org>
2019-03-13 15:30:12 +0100

    OPENEUROPA-1698: Add coding standard checks.

--- src/TaskRunner/Commands/ToolkitCommands.php
+++ src/TaskRunner/Commands/ToolkitCommands.php
@@ -1,16 +1,12 @@
 <?php
 
-namespace Eceuropa\Toolkit\TaskRunner\Commands;
+namespace EcEuropa\Toolkit\TaskRunner\Commands;
 
 use OpenEuropa\TaskRunner\Commands\AbstractCommands;
-use Consolidation\AnnotatedCommand\CommandData;
 use NuvoleWeb\Robo\Task as NuvoleWebTasks;
 use OpenEuropa\TaskRunner\Contract\FilesystemAwareInterface;
 use OpenEuropa\TaskRunner\Tasks as TaskRunnerTasks;
 use OpenEuropa\TaskRunner\Traits as TaskRunnerTraits;
-use Symfony\Component\Console\Event\ConsoleCommandEvent;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Yaml\Yaml;
 use GuzzleHttp\Client;
 
 /**
@@ -38,15 +34,15 @@ class ToolkitCommands extends AbstractCommands implements FilesystemAwareInterfa
      */
     public function toolkitClone()
     {
-      // Get updated dump if the case.
-      $this->toolkitDatabaseDownload();
+        // Get updated dump if the case.
+        $this->toolkitDatabaseDownload();
 
-      // Unzip and dump database file.
-      $this->taskExecStack()
-        ->stopOnFail()
-        ->exec('gunzip .tmp/dump.sql.gz')
-        ->exec('vendor/bin/drush --uri=web sqlc < .tmp/dump.sql')
-        ->run();
+        // Unzip and dump database file.
+        $this->taskExecStack()
+            ->stopOnFail()
+            ->exec('gunzip .tmp/dump.sql.gz')
+            ->exec('vendor/bin/drush --uri=web sqlc < .tmp/dump.sql')
+            ->run();
     }
 
     /**
@@ -59,37 +55,37 @@ class ToolkitCommands extends AbstractCommands implements FilesystemAwareInterfa
     public function toolkitDatabaseDownload()
     {
 
-      // Create folder if non-existent.
-      if (!is_dir('.tmp')) {
-        $this->taskExec('mkdir -p .tmp')->run();
-      }
-
-      $client = new Client();
-      $requestUrl = self::ASDA_URL . $this->config->get('project.id') . '/';
-      $requestOptions = [
-        'auth' => [
-          $this->config->get('asda.user'),
-          $this->config->get('asda.password'),
-        ]
-      ];
-
-      // Get current filename.
-      $response = $client->request('GET', $requestUrl, $requestOptions);
-      $body = (string) $response->getBody();
-
-      foreach(preg_split("/((\r?\n)|(\r\n?))/", trim(strip_tags($body))) as $key => $line){
-        $hashFilename = (strpos($line, '.sh')) ? trim($line) : 'latest.sh1';
-        $this->dumpFilename = (strpos($line, '.sql.gz')) ? trim($line) : '';
-      }
-
-      // Check if this files is already downloaded.
-      if (!is_file('.tmp/' . $this->dumpFilename)) {
-        // Download database.
-        if ($this->dumpFilename) {
-          $requestOptions += ['sink' => '.tmp/dump.sql.gz'];
-          $client->request('GET', $requestUrl . $this->dumpFilename, $requestOptions);
+        // Create folder if non-existent.
+        if (!is_dir('.tmp')) {
+            $this->taskExec('mkdir -p .tmp')->run();
         }
-      }
+
+        $client = new Client();
+        $requestUrl = self::ASDA_URL . $this->config->get('project.id') . '/';
+        $requestOptions = [
+            'auth' => [
+                $this->config->get('asda.user'),
+                $this->config->get('asda.password'),
+            ]
+        ];
+
+        // Get current filename.
+        $response = $client->request('GET', $requestUrl, $requestOptions);
+        $body = (string) $response->getBody();
+
+        foreach (preg_split("/((\r?\n)|(\r\n?))/", trim(strip_tags($body))) as $key => $line) {
+            $hashFilename = (strpos($line, '.sh')) ? trim($line) : 'latest.sh1';
+            $this->dumpFilename = (strpos($line, '.sql.gz')) ? trim($line) : '';
+        }
+
+        // Check if this files is already downloaded.
+        if (!is_file('.tmp/' . $this->dumpFilename)) {
+            // Download database.
+            if ($this->dumpFilename) {
+                $requestOptions += ['sink' => '.tmp/dump.sql.gz'];
+                $client->request('GET', $requestUrl . $this->dumpFilename, $requestOptions);
+            }
+        }
     }
 
     /**
@@ -100,7 +96,6 @@ class ToolkitCommands extends AbstractCommands implements FilesystemAwareInterfa
      */
     public function toolkitTestPhpcs()
     {
-      return $this->taskExec("./vendor/bin/grumphp run")->run();
+        return $this->taskExec("./vendor/bin/grumphp run")->run();
     }
-
 }
