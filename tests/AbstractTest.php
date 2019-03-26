11c5f4ad75031465a22842863bfb7aeb657ae0a2
Voidtek <vdacosta@voidtek.com>
2019-03-26 11:54:36 +0100

    OPENEUROPA-1741: Fix cs.

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -11,86 +11,95 @@ use Symfony\Component\Yaml\Yaml;
 /**
  * Abstract test class for Toolkit commands.
  */
-abstract class AbstractTest extends TestCase
-{
-    /**
-     * {@inheritdoc}
-     */
-    protected function setUp()
-    {
-        if (!is_dir($this->getSandboxRoot())) {
-            mkdir($this->getSandboxRoot());
-        }
-        $filesystem = new Filesystem();
-        $filesystem->chmod($this->getSandboxRoot(), 0777, umask(), true);
-        $filesystem->remove(glob($this->getSandboxRoot() . '/*'));
-    }
+abstract class AbstractTest extends TestCase {
 
-    /**
-     * Helper function to assert contain / not contain expectations.
-     *
-     * @param string $content
-     * @param array  $expected
-     */
-    protected function assertContainsNotContains($content, array $expected)
-    {
-        if (!empty($expected['contains'])) {
-            $this->assertContains($this->trimEachLine($expected['contains']), $this->trimEachLine($content));
-            $this->assertEquals(substr_count($this->trimEachLine($content), $this->trimEachLine($expected['contains'])), 1, 'String found more than once.');
-        }
-        if (!empty($expected['not_contains'])) {
-            $this->assertNotContains($this->trimEachLine($expected['not_contains']), $this->trimEachLine($content));
-        }
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    if (!is_dir($this->getSandboxRoot())) {
+      mkdir($this->getSandboxRoot());
     }
+    $filesystem = new Filesystem();
+    $filesystem->chmod($this->getSandboxRoot(), 0777, umask(), TRUE);
+    $filesystem->remove(glob($this->getSandboxRoot() . '/*'));
+  }
 
-    /**
-     * Trim each line of a blob of text, useful when asserting on multiline strings.
-     *
-     * @param string $text
-     *    Untrimmed text.
-     *
-     * @return string
-     *    Trimmed text.
-     */
-    protected function trimEachLine($text) {
-      return implode(PHP_EOL, array_map('trim', explode(PHP_EOL, $text)));
+  /**
+   * Helper function to assert contain / not contain expectations.
+   *
+   * @param string $content
+   *   Content to test.
+   * @param array $expected
+   *   Content expected.
+   */
+  protected function assertContainsNotContains($content, array $expected) {
+    if (!empty($expected['contains'])) {
+      $this->assertContains($this->trimEachLine($expected['contains']), $this->trimEachLine($content));
+      $this->assertEquals(substr_count($this->trimEachLine($content), $this->trimEachLine($expected['contains'])), 1, 'String found more than once.');
     }
+    if (!empty($expected['not_contains'])) {
+      $this->assertNotContains($this->trimEachLine($expected['not_contains']), $this->trimEachLine($content));
+    }
+  }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
-    protected function getClassLoader()
-    {
-        return require __DIR__ . '/../vendor/autoload.php';
-    }
+  /**
+   * Trim each line of a blob of text, used when asserting on multiline strings.
+   *
+   * @param string $text
+   *   Untrimmed text.
+   *
+   * @return string
+   *   Trimmed text.
+   */
+  protected function trimEachLine($text) {
+    return implode(PHP_EOL, array_map('trim', explode(PHP_EOL, $text)));
+  }
 
-    /**
-     * @param $filepath
-     *
-     * @return mixed
-     */
-    protected function getFixtureContent($filepath)
-    {
-        return Yaml::parse(file_get_contents(__DIR__ . "/fixtures/{$filepath}"));
-    }
+  /**
+   * Get class loader.
+   *
+   * @return \Composer\Autoload\ClassLoader
+   *   Class loader of vendor.
+   */
+  protected function getClassLoader() {
+    return require __DIR__ . '/../vendor/autoload.php';
+  }
 
-    /**
-     * @param $name
-     *
-     * @return string
-     */
-    protected function getSandboxFilepath($name)
-    {
-        return $this->getSandboxRoot() . '/' . $name;
-    }
+  /**
+   * Get fixture content.
+   *
+   * @param string $filepath
+   *   File path.
+   *
+   * @return mixed
+   *   A set of test data.
+   */
+  protected function getFixtureContent($filepath) {
+    return Yaml::parse(file_get_contents(__DIR__ . "/fixtures/{$filepath}"));
+  }
 
-    /**
-     * @param $name
-     *
-     * @return string
-     */
-    protected function getSandboxRoot()
-    {
-        return __DIR__ . '/sandbox';
-    }
+  /**
+   * Get sandbox filepath.
+   *
+   * @param string $name
+   *   A name of Sandbox.
+   *
+   * @return string
+   *   The filepath of the sandbox.
+   */
+  protected function getSandboxFilepath($name) {
+    return $this->getSandboxRoot() . '/' . $name;
+  }
+
+  /**
+   * Get sandbox root path.
+   *
+   * @return string
+   *   The filepath of sandbox.
+   */
+  protected function getSandboxRoot() {
+    return __DIR__ . '/sandbox';
+  }
+
 }
--- tests/Commands/DrupalCommandsTest.php
+++ tests/Commands/DrupalCommandsTest.php
@@ -13,54 +13,60 @@ use Symfony\Component\Yaml\Yaml;
 /**
  * Test Toolkit Drupal commands.
  */
-class DrupalCommandsTest extends AbstractTest
-{
-    /**
-     * @return array
-     */
-    public function drupalSettingsSetupDataProvider()
-    {
-        return $this->getFixtureContent('commands/drupal-settings-setup.yml');
+class DrupalCommandsTest extends AbstractTest {
+
+  /**
+   * Data provider for testDrupalSettingsSetup.
+   *
+   * @return array
+   *   An array of test data arrays with assertations.
+   */
+  public function dataProvider() {
+    return $this->getFixtureContent('commands/drupal-settings-setup.yml');
+  }
+
+  /**
+   * Test Toolkit very own "drupal:settings-setup" command.
+   *
+   * @param array $config
+   *   A configuration array.
+   * @param mixed $initial_default_settings
+   *   A initial default settings.
+   * @param mixed $initial_settings
+   *   A initial settings.
+   * @param array $expected
+   *   Test assertions.
+   *
+   * @dataProvider dataProvider
+   */
+  public function testDrupalSettingsSetup(array $config, $initial_default_settings, $initial_settings, array $expected) {
+    // Setup test Task Runner configuration file.
+    $configFile = $this->getSandboxFilepath('runner.yml');
+    file_put_contents($configFile, Yaml::dump($config));
+
+    // Setup test directory.
+    $sites_subdir = isset($config['drupal']['site']['sites_subdir']) ? $config['drupal']['site']['sites_subdir'] : 'default';
+    $settings_root = $this->getSandboxRoot() . '/build/sites/default';
+    mkdir($settings_root, 0777, TRUE);
+
+    // Setup initial default.settings.php and settings.php, if any.
+    file_put_contents($settings_root . '/default.settings.php', $initial_default_settings);
+
+    // Setup settings.php file, if test case requires it.
+    if ($initial_settings) {
+      file_put_contents($settings_root . '/settings.php', $initial_settings);
     }
 
-    /**
-     * Test Toolkit very own "drupal:settings-setup" command.
-     *
-     * @param array $config
-     * @param $initial_default_settings
-     * @param $initial_settings
-     * @param array $expected
-     *
-     * @dataProvider drupalSettingsSetupDataProvider
-     */
-    public function testDrupalSettingsSetup(array $config, $initial_default_settings, $initial_settings, array $expected)
-    {
-        // Setup test Task Runner configuration file.
-        $configFile = $this->getSandboxFilepath('runner.yml');
-        file_put_contents($configFile, Yaml::dump($config));
+    // Run command.
+    $input = new StringInput('drupal:settings-setup --working-dir=' . $this->getSandboxRoot());
+    $runner = new TaskRunner($input, new BufferedOutput(), $this->getClassLoader());
+    $runner->run();
 
-        // Setup test directory.
-        $sites_subdir = isset($config['drupal']['site']['sites_subdir']) ? $config['drupal']['site']['sites_subdir'] : 'default';
-        $settings_root = $this->getSandboxRoot() . '/build/sites/default';
-        mkdir($settings_root, 0777, true);
-
-        // Setup initial default.settings.php and settings.php, if any.
-        file_put_contents($settings_root . '/default.settings.php', $initial_default_settings);
-
-        // Setup settings.php file, if test case requires it.
-        if ($initial_settings) {
-            file_put_contents($settings_root . '/settings.php', $initial_settings);
-        }
-
-        // Run command.
-        $input = new StringInput('drupal:settings-setup --working-dir=' . $this->getSandboxRoot());
-        $runner = new TaskRunner($input, new BufferedOutput(), $this->getClassLoader());
-        $runner->run();
-
-        // Assert expectations.
-        foreach ($expected as $row) {
-            $content = file_get_contents($this->getSandboxFilepath($row['file']));
-            $this->assertContainsNotContains($content, $row);
-        }
+    // Assert expectations.
+    foreach ($expected as $row) {
+      $content = file_get_contents($this->getSandboxFilepath($row['file']));
+      $this->assertContainsNotContains($content, $row);
     }
+  }
+
 }
