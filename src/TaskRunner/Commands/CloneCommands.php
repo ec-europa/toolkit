6163abf9d61de357f7d9c9e5e08d9a85cf448cd2
Voidtek <vdacosta@voidtek.com>
2019-03-28 17:23:15 +0100

    OPENEUROPA-1746: Add Clone command

---
+++ src/TaskRunner/Commands/CloneCommands.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace EcEuropa\Toolkit\TaskRunner\Commands;
+
+use OpenEuropa\TaskRunner\Commands\AbstractCommands;
+use OpenEuropa\TaskRunner\Tasks as TaskRunnerTasks;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * Class ToolkitCommands.
+ */
+class CloneCommands extends AbstractCommands {
+
+  use TaskRunnerTasks\CollectionFactory\loadTasks;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfigurationFile() {
+    return __DIR__ . '/../../../config/commands/clone.yml';
+  }
+
+  /**
+   * Install clone from production snapshot.
+   *
+   * This will download the database if none local then proceed to dump and sync
+   * the configuration in the following order:
+   * - Verify if the dumpfile exists
+   * - Import dump.sql in the current installation
+   * - Execute cache-rebuild
+   * - Import configuration from datastore into activestore.
+   *
+   * @param array $options
+   *   Command options.
+   *
+   * @return \Robo\Collection\CollectionBuilder
+   *   Collection builder.
+   *
+   * @command toolkit:clone-site
+   *
+   * @option uri Drupal uri.
+   * @option dumpfile Drupal uri.
+   */
+  public function cloneSite(array $options = [
+    'uri' => InputOption::VALUE_REQUIRED,
+    'dumpfile' => InputOption::VALUE_REQUIRED,
+  ]) {
+    $tasks = [];
+
+    // Unzip and dump database file.
+    $tasks[] = $this->taskExecStack()
+      ->stopOnFail()
+      ->exec('vendor/bin/drush --uri=' . $options['uri'] . ' sql-drop')
+      ->exec('vendor/bin/drush --uri=' . $options['uri'] . ' sqlc < ' . $options['dumpfile'])
+      ->exec('vendor/bin/drush --uri=' . $options['uri'] . ' cim')
+      ->exec('vendor/bin/drush --uri=' . $options['uri'] . ' cr');
+
+    // Build and return task collection.
+    return $this->collectionBuilder()->addTaskList($tasks);
+  }
+
+}
--- src/TaskRunner/Commands/InstallCommands.php
+++ src/TaskRunner/Commands/InstallCommands.php
@@ -9,7 +9,6 @@ use NuvoleWeb\Robo\Task as NuvoleWebTasks;
 use OpenEuropa\TaskRunner\Contract\FilesystemAwareInterface;
 use OpenEuropa\TaskRunner\Tasks as TaskRunnerTasks;
 use OpenEuropa\TaskRunner\Traits as TaskRunnerTraits;
-use GuzzleHttp\Client;
 
 /**
  * Class ToolkitCommands.
@@ -19,43 +18,6 @@ class InstallCommands extends AbstractCommands implements FilesystemAwareInterfa
   use TaskRunnerTasks\CollectionFactory\loadTasks;
   use TaskRunnerTraits\ConfigurationTokensTrait;
   use TaskRunnerTraits\FilesystemAwareTrait;
-  const ASDA_URL = 'https://webgate.ec.europa.eu/fpfis/files-for/automate_dumps/';
-
-  public $dumpFilename = '';
-
-  /**
-   * Install clone from production snapshot.
-   *
-   * This will download the database if none local then proceed to dump and sync
-   * the configuration in the following order:
-   * - Verify if .tmp/dump.sql or dump.sql exists, if not download it
-   *   in .tmp/dump.sql
-   * - Import dump.sql in the current installation
-   * - Execute cache-rebuild
-   * - Check current status of configuration
-   * - Import configuration from datastore into activestore.
-   *
-   * @command toolkit:install-clone
-   */
-  public function clone() {
-    // Create folder if non-existent.
-    if (!is_file('./.tmp/dump.sql')) {
-      // Get updated dump if the case.
-      $this->databaseDownload();
-    }
-
-    // Unzip and dump database file.
-    $this->taskExecStack()
-      ->stopOnFail()
-      ->exec('gunzip .tmp/dump.sql.gz')
-      ->exec('vendor/bin/drush --uri=web sqlc < ./.tmp/dump.sql')
-      ->exec('vendor/bin/drush --uri=web cr')
-      ->exec('vendor/bin/drush --uri=web cst')
-      ->exec('vendor/bin/drush --uri=web cim -y')
-      ->run();
-
-    $this->disableDrupalCache();
-  }
 
   /**
    * Install clean website.
@@ -79,44 +41,6 @@ class InstallCommands extends AbstractCommands implements FilesystemAwareInterfa
       ->run();
 
     $this->disableDrupalCache();
-  }
-
-  /**
-   * Download production snapshot.
-   *
-   * @command toolkit:database-download
-   */
-  private function databaseDownload() {
-    // Create folder if non-existent.
-    if (!is_dir('.tmp')) {
-      $this->taskExec('mkdir -p .tmp')->run();
-    }
-
-    $client = new Client();
-    $requestUrl = self::ASDA_URL . $this->config->get('project.id') . '/';
-    $requestOptions = [
-      'auth' => [
-        $this->config->get('asda.user'),
-        $this->config->get('asda.password'),
-      ],
-    ];
-
-    // Get current filename.
-    $response = $client->request('GET', $requestUrl, $requestOptions);
-    $body = (string) $response->getBody();
-
-    foreach (preg_split("/((\r?\n)|(\r\n?))/", trim(strip_tags($body))) as $key => $line) {
-      $this->dumpFilename = (strpos($line, '.sql.gz')) ? trim($line) : '';
-    }
-
-    // Check if this files is already downloaded.
-    if (!is_file('.tmp/' . $this->dumpFilename)) {
-      // Download database.
-      if ($this->dumpFilename) {
-        $requestOptions += ['sink' => '.tmp/dump.sql.gz'];
-        $client->request('GET', $requestUrl . $this->dumpFilename, $requestOptions);
-      }
-    }
   }
 
   /**
