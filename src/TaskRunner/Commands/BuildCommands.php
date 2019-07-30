fd44fbaa945b312f98ee88ababb0b073d0abfda9
Alex Verbruggen <verbruggenalex@gmail.com>
2019-07-30 21:25:27 +0000

    Initial draft.

--- src/TaskRunner/Commands/BuildCommands.php
+++ src/TaskRunner/Commands/BuildCommands.php
@@ -51,27 +51,45 @@ class BuildCommands extends AbstractCommands {
     'dist-root' => InputOption::VALUE_REQUIRED,
   ]) {
     $tasks = [];
-
-    // Reset dist folder and copy required files.
+    $prepDir = '.tmp/dist/prep';
+    
+    // Create temp folder to prepare dist build in.
     $tasks[] = $this->taskFilesystemStack()
-      ->remove($options['dist-root'])
-      ->mkdir($options['dist-root'])
-      ->copy('./composer.json', $options['dist-root'] . '/composer.json')
-      ->copy('./composer.lock', $options['dist-root'] . '/composer.lock');
+      ->remove($prepDir)
+      ->mkdir($prepDir);
 
-    // Copy site configuration.
-    $tasks[] = $this->taskCopyDir(['./config' => $options['dist-root'] . '/config']);
+    // Rsync the codebase to the tmp folder.
+    $tasks[] = $this->taskRsync()
+      ->fromPath('./')
+      ->toPath($prepDir)
+      ->exclude(['.tmp', 'vendor'])
+      ->excludeVcs()
+      ->recursive();
 
     // Run production-friendly "composer install" packages.
     $tasks[] = $this->taskComposerInstall('composer')
-      ->workingDir($options['dist-root'])
+      ->workingDir($prepDir)
       ->optimizeAutoloader()
       ->noDev();
 
-    // Setup the site.
-    $tasks[] = $this->taskExecStack()
-      ->stopOnFail()
-      ->exec('./vendor/bin/run drupal:settings-setup --root=' . $options['dist-root'] . '/' . $options['root']);
+    // // Setup the site.
+    // $tasks[] = $this->taskExecStack()
+    //   ->stopOnFail()
+    //   ->exec('./vendor/bin/run drupal:settings-setup --root=' . $prepDir. '/' . $options['root']);
+
+    // Create temp folder to prepare dist build in.
+    $tasks[] = $this->taskFilesystemStack()
+      ->remove($options['dist-root'])
+      ->mkdir($options['dist-root']);
+
+    // Rsync the codebase to the tmp folder.
+    $tasks[] = $this->taskRsync()
+      ->fromPath($prepDir . '/')
+      ->toPath($options['dist-root'])
+      ->includeFilter(['composer.*', 'config/***', 'drush/***', 'vendor/***', 'web/***'])
+      ->exclude('*')
+      ->recursive()
+      ->args('-aL');
 
     // Collect and execute list of commands set on local runner.yml.
     $commands = $this->getConfig()->get("toolkit.build.dist.commands");
