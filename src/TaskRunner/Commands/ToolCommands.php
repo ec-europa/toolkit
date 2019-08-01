b34c3a9851747f1eb4842850c337e233355f6570
Alex Verbruggen <verbruggenalex@gmail.com>
2019-08-01 07:37:20 +0000

    MULTISITE-21780: First draft with accept url.

--- src/TaskRunner/Commands/ToolCommands.php
+++ src/TaskRunner/Commands/ToolCommands.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace EcEuropa\Toolkit\TaskRunner\Commands;
 
+use Symfony\Component\Console\Input\InputOption;
 use OpenEuropa\TaskRunner\Commands\AbstractCommands;
 
 /**
@@ -30,6 +31,44 @@ class ToolCommands extends AbstractCommands {
 
     // Build and return task collection.
     return $this->collectionBuilder()->addTaskList($tasks);
+  }
+
+  /**
+   * Display toolkit notifications.
+   *
+   * @command toolkit:notifications
+   *
+   * @return \Robo\Collection\CollectionBuilder
+   *   Collection builder.
+   * @option endpoint-url The endpoint for the notifications
+   */
+  public function displayNotifications(array $options = [
+    'endpoint-url' => InputOption::VALUE_OPTIONAL,
+  ]) {
+    $endpointUrl = isset($options['endpoint-url']) ? $options['endpoint-url'] : 'https://webgate.acceptance.ec.europa.eu/fpfis/qa/api/v1/notifications';
+
+    if (isset($endpointUrl)) {
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $endpointUrl);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      $result = curl_exec($ch);
+  
+      // If request did not fail.
+      if ($result !== false) {
+        // Request was ok? check response code.
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($statusCode == 200) {
+          $data = json_decode($result, true);
+          foreach ($data as $notification) {
+            $this->io()->warning($notification['notification']);
+          }
+        }
+        else {
+          $this->io()->error(sprintf('Curl request failed with error code %d. Skipping notification fetching.', $statusCode));
+        }
+      }
+      curl_close($ch);
+    }//end if
   }
 
 }
