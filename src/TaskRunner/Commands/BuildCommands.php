f7952e89e5e91a7606cbb905b60c7645bc04ca12
Alex Verbruggen <verbruggenalex@gmail.com>
2019-08-07 07:57:47 +0000

    MULTISITE-21582: Add permissions setup for settings.php.

--- src/TaskRunner/Commands/BuildCommands.php
+++ src/TaskRunner/Commands/BuildCommands.php
@@ -76,6 +76,7 @@ class BuildCommands extends AbstractCommands {
     // Setup the site.
     $tasks[] = $this->taskExecStack()
       ->stopOnFail()
+      ->exec('./vendor/bin/run drupal:permissions-setup --root=' . $prepDir . '/' . $options['root'])
       ->exec('./vendor/bin/run drupal:settings-setup --root=' . $prepDir . '/' . $options['root']);
 
     // Create temp folder to prepare dist build in.
