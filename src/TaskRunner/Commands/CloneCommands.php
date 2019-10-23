d33bea3cb831e6538691c8c73906588cc538f9c8
Alex Verbruggen <verbruggenalex@gmail.com>
2019-10-23 08:45:32 +0000

    MULTISITE-22243: Re-run grumphp.

--- src/TaskRunner/Commands/CloneCommands.php
+++ src/TaskRunner/Commands/CloneCommands.php
@@ -18,296 +18,296 @@ use Symfony\Component\Console\Input\InputOption;
 class CloneCommands extends AbstractCommands
 {
 
-    use TaskRunnerTasks\CollectionFactory\loadTasks;
+use TaskRunnerTasks\CollectionFactory\loadTasks;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfigurationFile()
-    {
-        return __DIR__ . '/../../../config/commands/clone.yml';
-    }
+/**
+* {@inheritdoc}
+*/
+public function getConfigurationFile()
+{
+return __DIR__ . '/../../../config/commands/clone.yml';
+}
 
-    /**
-     * Install clone from production snapshot.
-     *
-     * It restores the database and imports the configuration.
-     * - Verify if the dumpfile exists.
-     * - Import configuration from sync into active storage.
-     * - Execute cache-rebuild.
-     *
-     * @param array $options
-     *   Command options.
-     *
-     * @return \Robo\Collection\CollectionBuilder
-     *   Collection builder.
-     *
-     * @command toolkit:install-dump
-     *
-     * @option dumpfile Drupal uri.
-     */
-    public function installDump(array $options = [
-        'dumpfile' => InputOption::VALUE_REQUIRED,
-    ])
-    {
-        $tasks = [];
+/**
+* Install clone from production snapshot.
+*
+* It restores the database and imports the configuration.
+* - Verify if the dumpfile exists.
+* - Import configuration from sync into active storage.
+* - Execute cache-rebuild.
+*
+* @param array $options
+*   Command options.
+*
+* @return \Robo\Collection\CollectionBuilder
+*   Collection builder.
+*
+* @command toolkit:install-dump
+*
+* @option dumpfile Drupal uri.
+*/
+public function installDump(array $options = [
+'dumpfile' => InputOption::VALUE_REQUIRED,
+])
+{
+$tasks = [];
 
-        if (!file_exists($options['dumpfile'])) {
-            $this->say('"' . $options['dumpfile'] . '" file not found, use the command "toolkit:download-dump --dumpfile ' . $options['dumpfile'] . '".');
+if (!file_exists($options['dumpfile'])) {
+$this->say('"' . $options['dumpfile'] . '" file not found, use the command "toolkit:download-dump --dumpfile ' . $options['dumpfile'] . '".');
 
-            return $this->collectionBuilder()->addTaskList($tasks);
-        }
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 
-        // Unzip and dump database file.
-        $tasks[] = $this->taskExecStack()
-            ->stopOnFail()
-            ->exec('./vendor/bin/drush sql-drop -y')
-            ->exec('./vendor/bin/drush sql-create -y')
-            ->exec('./vendor/bin/drush sqlc < ' . $options['dumpfile'])
-            ->exec('./vendor/bin/drush updatedb -y')
-            ->exec('./vendor/bin/drush cache:rebuild');
+// Unzip and dump database file.
+$tasks[] = $this->taskExecStack()
+->stopOnFail()
+->exec('./vendor/bin/drush sql-drop -y')
+->exec('./vendor/bin/drush sql-create -y')
+->exec('./vendor/bin/drush sqlc < ' . $options['dumpfile'])
+->exec('./vendor/bin/drush updatedb -y')
+->exec('./vendor/bin/drush cache:rebuild');
 
-        // Build and return task collection.
-        return $this->collectionBuilder()->addTaskList($tasks);
+// Build and return task collection.
+return $this->collectionBuilder()->addTaskList($tasks);
 =======
 class CloneCommands extends AbstractCommands {
 
-  use TaskRunnerTasks\CollectionFactory\loadTasks;
+use TaskRunnerTasks\CollectionFactory\loadTasks;
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getConfigurationFile() {
-    return __DIR__ . '/../../../config/commands/clone.yml';
-  }
+/**
+* {@inheritdoc}
+*/
+public function getConfigurationFile() {
+return __DIR__ . '/../../../config/commands/clone.yml';
+}
 
-  /**
-   * Run deployment sequence.
-   *
-   * This command will check for a file that holds the deployment sequence. If
-   * it is available it will run the commands defined in the yaml file under the
-   * selected key. If not we will run a standard set of deployment commands.
-   *
-   * @param array $options
-   *   Command options.
-   *
-   * @return \Robo\Collection\CollectionBuilder
-   *   Collection builder.
-   *
-   * @command toolkit:run-deploy
-   *
-   * @option sequence-file  The file that holds the deployment sequence.
-   * @option sequence-key   The key under which the commands are defined.
-   * @option config-file    The config file that triggers the config import.
-   */
-  public function runDeploy(array $options = [
-    'sequence-file' => InputOption::VALUE_REQUIRED,
-    'sequence-key' => InputOption::VALUE_REQUIRED,
-    'config-file' => InputOption::VALUE_REQUIRED,
-  ]) {
-    $tasks = [];
+/**
+* Run deployment sequence.
+*
+* This command will check for a file that holds the deployment sequence. If
+* it is available it will run the commands defined in the yaml file under the
+* selected key. If not we will run a standard set of deployment commands.
+*
+* @param array $options
+*   Command options.
+*
+* @return \Robo\Collection\CollectionBuilder
+*   Collection builder.
+*
+* @command toolkit:run-deploy
+*
+* @option sequence-file  The file that holds the deployment sequence.
+* @option sequence-key   The key under which the commands are defined.
+* @option config-file    The config file that triggers the config import.
+*/
+public function runDeploy(array $options = [
+'sequence-file' => InputOption::VALUE_REQUIRED,
+'sequence-key' => InputOption::VALUE_REQUIRED,
+'config-file' => InputOption::VALUE_REQUIRED,
+]) {
+$tasks = [];
 
-    $has_config = file_exists($options['config-file']);
-    $has_sequence = file_exists($options['sequence-file']);
+$has_config = file_exists($options['config-file']);
+$has_sequence = file_exists($options['sequence-file']);
 
-    if ($has_sequence) {
-      $config = new Config();
-      $loader = new YamlConfigLoader();
-      $processor = new ConfigProcessor();
-      $processor->extend($loader->load($options['sequence-file']));
-      $config->import($processor->export());
-      $sequence = $config->get($options['sequence-key']);
+if ($has_sequence) {
+$config = new Config();
+$loader = new YamlConfigLoader();
+$processor = new ConfigProcessor();
+$processor->extend($loader->load($options['sequence-file']));
+$config->import($processor->export());
+$sequence = $config->get($options['sequence-key']);
 
-      if (!empty($sequence)) {
-        $this->say('Running custom deploy sequence "' . $options['sequence-key'] . '" from sequence file "' . $options['sequence-file'] . '".');
-        foreach ($sequence as $command) {
-          $tasks[] = $this->taskExec($command);
-        }
-        return $this->collectionBuilder()->addTaskList($tasks);
-      }
-      else {
-        $this->say('Sequence key "' . $options['sequence-key'] . '" does not contain commands, running default set of deployment commands.');
-      }
-    }
-    else {
-      $this->say('Sequence file "' . $options['sequence-file'] . '" does not exist, running default set of deployment commands.');
-    }
+if (!empty($sequence)) {
+$this->say('Running custom deploy sequence "' . $options['sequence-key'] . '" from sequence file "' . $options['sequence-file'] . '".');
+foreach ($sequence as $command) {
+$tasks[] = $this->taskExec($command);
+}
+return $this->collectionBuilder()->addTaskList($tasks);
+}
+else {
+$this->say('Sequence key "' . $options['sequence-key'] . '" does not contain commands, running default set of deployment commands.');
+}
+}
+else {
+$this->say('Sequence file "' . $options['sequence-file'] . '" does not exist, running default set of deployment commands.');
+}
 
-    // Default deployment sequence.
-    $tasks[] = $this->taskExec('./vendor/bin/drush state:set system.maintenance_mode 1 --input-format=integer -y');
-    $tasks[] = $this->taskExec('./vendor/bin/drush updatedb -y');
-    if ($has_config) {
-      $tasks[] = $this->taskExec('./vendor/bin/run toolkit:import-config');
-    }
-    $tasks[] = $this->taskExec('./vendor/bin/drush state:set system.maintenance_mode 0 --input-format=integer -y');
-    $tasks[] = $this->taskExec('./vendor/bin/drush cache:rebuild');
+// Default deployment sequence.
+$tasks[] = $this->taskExec('./vendor/bin/drush state:set system.maintenance_mode 1 --input-format=integer -y');
+$tasks[] = $this->taskExec('./vendor/bin/drush updatedb -y');
+if ($has_config) {
+$tasks[] = $this->taskExec('./vendor/bin/run toolkit:import-config');
+}
+$tasks[] = $this->taskExec('./vendor/bin/drush state:set system.maintenance_mode 0 --input-format=integer -y');
+$tasks[] = $this->taskExec('./vendor/bin/drush cache:rebuild');
 
-    return $this->collectionBuilder()->addTaskList($tasks);
-  }
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 
-  /**
-   * Install clone from production snapshot.
-   *
-   * It restores the database and imports the configuration.
-   * - Verify if the dumpfile exists.
-   * - Import configuration from sync into active storage.
-   * - Execute cache-rebuild.
-   *
-   * @param array $options
-   *   Command options.
-   *
-   * @return \Robo\Collection\CollectionBuilder
-   *   Collection builder.
-   *
-   * @command toolkit:install-dump
-   *
-   * @option dumpfile Drupal uri.
-   */
-  public function installDump(array $options = [
-    'dumpfile' => InputOption::VALUE_REQUIRED,
-  ]) {
-    $tasks = [];
+/**
+* Install clone from production snapshot.
+*
+* It restores the database and imports the configuration.
+* - Verify if the dumpfile exists.
+* - Import configuration from sync into active storage.
+* - Execute cache-rebuild.
+*
+* @param array $options
+*   Command options.
+*
+* @return \Robo\Collection\CollectionBuilder
+*   Collection builder.
+*
+* @command toolkit:install-dump
+*
+* @option dumpfile Drupal uri.
+*/
+public function installDump(array $options = [
+'dumpfile' => InputOption::VALUE_REQUIRED,
+]) {
+$tasks = [];
 
-    if (!file_exists($options['dumpfile'])) {
-      $this->say('"' . $options['dumpfile'] . '" file not found, use the command "toolkit:download-dump --dumpfile ' . $options['dumpfile'] . '".');
+if (!file_exists($options['dumpfile'])) {
+$this->say('"' . $options['dumpfile'] . '" file not found, use the command "toolkit:download-dump --dumpfile ' . $options['dumpfile'] . '".');
 
-      return $this->collectionBuilder()->addTaskList($tasks);
-    }
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 
-    // Unzip and dump database file.
-    $tasks[] = $this->taskExecStack()
-      ->stopOnFail()
-      ->exec('./vendor/bin/drush sql-drop -y')
-      ->exec('./vendor/bin/drush sql-create -y')
-      ->exec('./vendor/bin/drush sqlc < ' . $options['dumpfile']);
+// Unzip and dump database file.
+$tasks[] = $this->taskExecStack()
+->stopOnFail()
+->exec('./vendor/bin/drush sql-drop -y')
+->exec('./vendor/bin/drush sql-create -y')
+->exec('./vendor/bin/drush sqlc < ' . $options['dumpfile']);
 
-    // Build and return task collection.
-    return $this->collectionBuilder()->addTaskList($tasks);
-  }
+// Build and return task collection.
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 
-  /**
-   * Download ASDA snapshot.
-   *
-   * In order to make use of this functionality you must add your
-   * ASDA credentials to your environment like.
-   *
-   * @param array $options
-   *   Command options.
-   *
-   * @command toolkit:download-dump
-   *
-   * @return \Robo\Collection\CollectionBuilder|void
-   *   Collection builder.
-   */
-  public function downloadDump(array $options = [
-    'asda-url' => InputOption::VALUE_REQUIRED,
-    'asda-user' => InputOption::VALUE_REQUIRED,
-    'asda-password' => InputOption::VALUE_REQUIRED,
-    'dumpfile' => InputOption::VALUE_REQUIRED,
-    'project-id' => InputOption::VALUE_REQUIRED,
-  ]) {
-    $tasks = [];
+/**
+* Download ASDA snapshot.
+*
+* In order to make use of this functionality you must add your
+* ASDA credentials to your environment like.
+*
+* @param array $options
+*   Command options.
+*
+* @command toolkit:download-dump
+*
+* @return \Robo\Collection\CollectionBuilder|void
+*   Collection builder.
+*/
+public function downloadDump(array $options = [
+'asda-url' => InputOption::VALUE_REQUIRED,
+'asda-user' => InputOption::VALUE_REQUIRED,
+'asda-password' => InputOption::VALUE_REQUIRED,
+'dumpfile' => InputOption::VALUE_REQUIRED,
+'project-id' => InputOption::VALUE_REQUIRED,
+]) {
+$tasks = [];
 
-    // Check credentials.
-    if ($options['asda-user'] === '${env.ASDA_USER}' || $options['asda-password'] === '${env.ASDA_PASSWORD}') {
-      $this->say('ASDA credentials not found, set them as the following environment variables: ASDA_USER, ASDA_PASSWORD.');
+// Check credentials.
+if ($options['asda-user'] === '${env.ASDA_USER}' || $options['asda-password'] === '${env.ASDA_PASSWORD}') {
+$this->say('ASDA credentials not found, set them as the following environment variables: ASDA_USER, ASDA_PASSWORD.');
 
-      return $this->collectionBuilder()->addTaskList($tasks);
+return $this->collectionBuilder()->addTaskList($tasks);
 >>>>>>> release/4.x
-    }
+}
 
-    /**
-     * Download ASDA snapshot.
-     *
-     * In order to make use of this functionality you must add your
-     * ASDA credentials to your environment like.
-     *
-     * @param array $options
-     *   Command options.
-     *
-     * @command toolkit:download-dump
-     *
-     * @return \Robo\Collection\CollectionBuilder|void
-     *   Collection builder.
-     */
-    public function downloadDump(array $options = [
-        'asda-url' => InputOption::VALUE_REQUIRED,
-        'asda-user' => InputOption::VALUE_REQUIRED,
-        'asda-password' => InputOption::VALUE_REQUIRED,
-        'dumpfile' => InputOption::VALUE_REQUIRED,
-        'project-id' => InputOption::VALUE_REQUIRED,
-    ])
-    {
-        $tasks = [];
+/**
+* Download ASDA snapshot.
+*
+* In order to make use of this functionality you must add your
+* ASDA credentials to your environment like.
+*
+* @param array $options
+*   Command options.
+*
+* @command toolkit:download-dump
+*
+* @return \Robo\Collection\CollectionBuilder|void
+*   Collection builder.
+*/
+public function downloadDump(array $options = [
+'asda-url' => InputOption::VALUE_REQUIRED,
+'asda-user' => InputOption::VALUE_REQUIRED,
+'asda-password' => InputOption::VALUE_REQUIRED,
+'dumpfile' => InputOption::VALUE_REQUIRED,
+'project-id' => InputOption::VALUE_REQUIRED,
+])
+{
+$tasks = [];
 
-        // Check credentials.
-        if ($options['asda-user'] === '${env.ASDA_USER}' || $options['asda-password'] === '${env.ASDA_PASSWORD}') {
-            $this->say('ASDA credentials not found, set them as the following environment variables: ASDA_USER, ASDA_PASSWORD.');
+// Check credentials.
+if ($options['asda-user'] === '${env.ASDA_USER}' || $options['asda-password'] === '${env.ASDA_PASSWORD}') {
+$this->say('ASDA credentials not found, set them as the following environment variables: ASDA_USER, ASDA_PASSWORD.');
 
-            return $this->collectionBuilder()->addTaskList($tasks);
-        }
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 
-        $requestUrl = $options['asda-url'] . '/' . $options['project-id'];
+$requestUrl = $options['asda-url'] . '/' . $options['project-id'];
 
-        // Download the file.
-        $this->downloadChecksumFile($options);
-        $fileContent = file_get_contents('latest.sh1');
-        $filename = trim(explode('  ', $fileContent)[1]);
+// Download the file.
+$this->downloadChecksumFile($options);
+$fileContent = file_get_contents('latest.sh1');
+$filename = trim(explode('  ', $fileContent)[1]);
 
-        // Download the file.
-        $tasks[] = $this->taskExec('wget')
-            ->option('--http-user', $options['asda-user'])
-            ->option('--http-password', $options['asda-password'])
-            ->option('-O', $options['dumpfile'] . '.gz')
-            ->option('-nH', $requestUrl . '/' . $filename)
-            ->option('-A', 'sql.gz')
-            ->option('-P', './');
+// Download the file.
+$tasks[] = $this->taskExec('wget')
+->option('--http-user', $options['asda-user'])
+->option('--http-password', $options['asda-password'])
+->option('-O', $options['dumpfile'] . '.gz')
+->option('-nH', $requestUrl . '/' . $filename)
+->option('-A', 'sql.gz')
+->option('-P', './');
 
-        // Unzip the file.
-        $tasks[] = $this->taskExec('gunzip')
-            ->arg($options['dumpfile'] . '.gz');
+// Unzip the file.
+$tasks[] = $this->taskExec('gunzip')
+->arg($options['dumpfile'] . '.gz');
 
-        // Remove checkum file.
-        $tasks[] = $this->taskExec('rm')
-            ->arg('latest.sh1');
+// Remove checkum file.
+$tasks[] = $this->taskExec('rm')
+->arg('latest.sh1');
 
-        // Build and return task collection.
-        return $this->collectionBuilder()->addTaskList($tasks);
-    }
+// Build and return task collection.
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 
-    /**
-     * Download Checksum file.
-     *
-     * Make use checksum file in order to detect the proper file
-     * to download.
-     *
-     * @param array $options
-     *   Command options.
-     */
-    private function downloadChecksumFile(array $options = [
-        'asda-url' => InputOption::VALUE_REQUIRED,
-        'asda-user' => InputOption::VALUE_REQUIRED,
-        'asda-password' => InputOption::VALUE_REQUIRED,
-        'dumpfile' => InputOption::VALUE_REQUIRED,
-        'project-id' => InputOption::VALUE_REQUIRED,
-    ])
-    {
-        $tmpDir = $this->getConfig()->get("toolkit.tmp_folder");
+/**
+* Download Checksum file.
+*
+* Make use checksum file in order to detect the proper file
+* to download.
+*
+* @param array $options
+*   Command options.
+*/
+private function downloadChecksumFile(array $options = [
+'asda-url' => InputOption::VALUE_REQUIRED,
+'asda-user' => InputOption::VALUE_REQUIRED,
+'asda-password' => InputOption::VALUE_REQUIRED,
+'dumpfile' => InputOption::VALUE_REQUIRED,
+'project-id' => InputOption::VALUE_REQUIRED,
+])
+{
+$tmpDir = $this->getConfig()->get("toolkit.tmp_folder");
 
-        // Create temp folder to prepare dist build in.
-        $tasks[] = $this->taskFilesystemStack()
-            ->mkdir($tmpDir)
-            ->run();
+// Create temp folder to prepare dist build in.
+$tasks[] = $this->taskFilesystemStack()
+->mkdir($tmpDir)
+->run();
 
-        $requestUrl = $options['asda-url'] . '/' . $options['project-id'];
+$requestUrl = $options['asda-url'] . '/' . $options['project-id'];
 
-        $this->taskExec('wget')
-            ->option('--http-user', $options['asda-user'])
-            ->option('--http-password', $options['asda-password'])
-            ->option('-O', 'latest.sh1')
-            ->option('-nH', $requestUrl . '/latest.sh1')
-            ->option('-A', '.sh1')
-            ->option('-P', './')
-            ->run();
-    }
+$this->taskExec('wget')
+->option('--http-user', $options['asda-user'])
+->option('--http-password', $options['asda-password'])
+->option('-O', 'latest.sh1')
+->option('-nH', $requestUrl . '/latest.sh1')
+->option('-A', '.sh1')
+->option('-P', './')
+->run();
+}
 }
--- src/TaskRunner/Commands/InstallCommands.php
+++ src/TaskRunner/Commands/InstallCommands.php
@@ -15,158 +15,158 @@ use Symfony\Component\Console\Input\InputOption;
 class InstallCommands extends AbstractCommands
 {
 
-    use TaskRunnerTasks\Drush\loadTasks;
+use TaskRunnerTasks\Drush\loadTasks;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfigurationFile()
-    {
-        return __DIR__ . '/../../../config/commands/install.yml';
-    }
+/**
+* {@inheritdoc}
+*/
+public function getConfigurationFile()
+{
+return __DIR__ . '/../../../config/commands/install.yml';
+}
 
-    /**
-     * Install a clean website.
-     *
-     * @param array $options
-     *   Command options.
-     *
-     * @command toolkit:install-clean
-     *
-     * @return \Robo\Collection\CollectionBuilder
-     *   Collection builder.
-     */
-    public function installClean(array $options = [
-        'config-file' => InputOption::VALUE_REQUIRED,
-    ])
-    {
-        $tasks = [];
+/**
+* Install a clean website.
+*
+* @param array $options
+*   Command options.
+*
+* @command toolkit:install-clean
+*
+* @return \Robo\Collection\CollectionBuilder
+*   Collection builder.
+*/
+public function installClean(array $options = [
+'config-file' => InputOption::VALUE_REQUIRED,
+])
+{
+$tasks = [];
 
-        // Install site from existing configuration, if available.
-        $has_config = file_exists($options['config-file']);
-        $params = $has_config ? ' --existing-config' : '';
+// Install site from existing configuration, if available.
+$has_config = file_exists($options['config-file']);
+$params = $has_config ? ' --existing-config' : '';
 
-        $tasks[] = $this->taskExecStack()
-            ->stopOnFail()
-            ->exec('./vendor/bin/run toolkit:build-dev')
-            ->exec('./vendor/bin/run drupal:site-install' . $params);
+$tasks[] = $this->taskExecStack()
+->stopOnFail()
+->exec('./vendor/bin/run toolkit:build-dev')
+->exec('./vendor/bin/run drupal:site-install' . $params);
 
-        // Build and return task collection.
-        return $this->collectionBuilder()->addTaskList($tasks);
-    }
+// Build and return task collection.
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 =======
 class InstallCommands extends AbstractCommands {
 
-  use TaskRunnerTasks\Drush\loadTasks;
+use TaskRunnerTasks\Drush\loadTasks;
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getConfigurationFile() {
-    return __DIR__ . '/../../../config/commands/install.yml';
-  }
+/**
+* {@inheritdoc}
+*/
+public function getConfigurationFile() {
+return __DIR__ . '/../../../config/commands/install.yml';
+}
 
-  /**
-   * Install a clean website.
-   *
-   * @param array $options
-   *   Command options.
-   *
-   * @command toolkit:install-clean
-   *
-   * @return \Robo\Collection\CollectionBuilder
-   *   Collection builder.
-   */
-  public function installClean(array $options = [
-    'config-file' => InputOption::VALUE_REQUIRED,
-  ]) {
-    $tasks = [];
+/**
+* Install a clean website.
+*
+* @param array $options
+*   Command options.
+*
+* @command toolkit:install-clean
+*
+* @return \Robo\Collection\CollectionBuilder
+*   Collection builder.
+*/
+public function installClean(array $options = [
+'config-file' => InputOption::VALUE_REQUIRED,
+]) {
+$tasks = [];
 
-    // Install site from existing configuration, if available.
-    $has_config = file_exists($options['config-file']);
-    $params = $has_config ? ' --existing-config' : '';
+// Install site from existing configuration, if available.
+$has_config = file_exists($options['config-file']);
+$params = $has_config ? ' --existing-config' : '';
 
-    $tasks[] = $this->taskExecStack()
-      ->stopOnFail()
-      ->exec('./vendor/bin/run toolkit:build-dev')
-      ->exec('./vendor/bin/run drupal:site-install' . $params);
+$tasks[] = $this->taskExecStack()
+->stopOnFail()
+->exec('./vendor/bin/run toolkit:build-dev')
+->exec('./vendor/bin/run drupal:site-install' . $params);
 
-    // Build and return task collection.
-    return $this->collectionBuilder()->addTaskList($tasks);
-  }
+// Build and return task collection.
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 
-  /**
-   * Install a clone website.
-   *
-   * @command toolkit:install-clone
-   *
-   * @return \Robo\Collection\CollectionBuilder
-   *   Collection builder.
-   */
-  public function installClone() {
-    $tasks = [];
+/**
+* Install a clone website.
+*
+* @command toolkit:install-clone
+*
+* @return \Robo\Collection\CollectionBuilder
+*   Collection builder.
+*/
+public function installClone() {
+$tasks = [];
 
-    $tasks[] = $this->taskExec('./vendor/bin/run toolkit:install-dump');
-    $tasks[] = $this->taskExec('./vendor/bin/run toolkit:run-deploy');
+$tasks[] = $this->taskExec('./vendor/bin/run toolkit:install-dump');
+$tasks[] = $this->taskExec('./vendor/bin/run toolkit:run-deploy');
 >>>>>>> release/4.x
 
-    /**
-     * Install a clone website.
-     *
-     * @param array $options
-     *   Command options.
-     *
-     * @command toolkit:install-clone
-     *
-     * @return \Robo\Collection\CollectionBuilder
-     *   Collection builder.
-     */
-    public function installClone(array $options = [
-        'dumpfile' => InputOption::VALUE_REQUIRED,
-        'config-file' => InputOption::VALUE_REQUIRED,
-    ])
-    {
-        $tasks = [];
+/**
+* Install a clone website.
+*
+* @param array $options
+*   Command options.
+*
+* @command toolkit:install-clone
+*
+* @return \Robo\Collection\CollectionBuilder
+*   Collection builder.
+*/
+public function installClone(array $options = [
+'dumpfile' => InputOption::VALUE_REQUIRED,
+'config-file' => InputOption::VALUE_REQUIRED,
+])
+{
+$tasks = [];
 
-        $has_dump = file_exists($options['dumpfile']);
-        $has_config = file_exists($options['config-file']);
+$has_dump = file_exists($options['dumpfile']);
+$has_config = file_exists($options['config-file']);
 
-        // If ASDA snapshot is available then we will restore it.
-        if ($has_dump) {
-            $tasks[] = $this->taskExec('./vendor/bin/run toolkit:install-dump');
+// If ASDA snapshot is available then we will restore it.
+if ($has_dump) {
+$tasks[] = $this->taskExec('./vendor/bin/run toolkit:install-dump');
 
-            // If also configuration is present then we will import it.
-            if ($has_config) {
-                $tasks[] = $this->taskExec('./vendor/bin/run toolkit:import-config');
-            }
-        } else {
-            // Install site from existing configuration, if available.
-            $params = $has_config ? ' --existing-config' : '';
-            $tasks[] = $this->taskExec('./vendor/bin/run drupal:site-install' . $params);
-        }
+// If also configuration is present then we will import it.
+if ($has_config) {
+$tasks[] = $this->taskExec('./vendor/bin/run toolkit:import-config');
+}
+} else {
+// Install site from existing configuration, if available.
+$params = $has_config ? ' --existing-config' : '';
+$tasks[] = $this->taskExec('./vendor/bin/run drupal:site-install' . $params);
+}
 
-        // Build and return task collection.
-        return $this->collectionBuilder()->addTaskList($tasks);
-    }
+// Build and return task collection.
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 
-    /**
-     * Import config.
-     *
-     * @command toolkit:import-config
-     *
-     * @return \Robo\Collection\CollectionBuilder
-     *   Collection builder.
-     */
-    public function importConfig()
-    {
-        $tasks = [];
+/**
+* Import config.
+*
+* @command toolkit:import-config
+*
+* @return \Robo\Collection\CollectionBuilder
+*   Collection builder.
+*/
+public function importConfig()
+{
+$tasks = [];
 
-        $tasks[] = $this->taskExecStack()
-            ->stopOnFail()
-            ->exec('./vendor/bin/drush config:import -y')
-            ->exec('./vendor/bin/drush cache:rebuild');
+$tasks[] = $this->taskExecStack()
+->stopOnFail()
+->exec('./vendor/bin/drush config:import -y')
+->exec('./vendor/bin/drush cache:rebuild');
 
-        // Build and return task collection.
-        return $this->collectionBuilder()->addTaskList($tasks);
-    }
+// Build and return task collection.
+return $this->collectionBuilder()->addTaskList($tasks);
+}
 }
