87242fd495e0643bf871afc186f8d1a851ba5161
Alex Verbruggen <verbruggenalex@gmail.com>
2019-12-04 08:48:05 +0000

    MULTISITE-22486: Cleanup and return 1 on error.

--- src/TaskRunner/Commands/TestsCommands.php
+++ src/TaskRunner/Commands/TestsCommands.php
@@ -9,6 +9,7 @@ use NuvoleWeb\Robo\Task as NuvoleWebTasks;
 use OpenEuropa\TaskRunner\Contract\FilesystemAwareInterface;
 use OpenEuropa\TaskRunner\Tasks as TaskRunnerTasks;
 use OpenEuropa\TaskRunner\Traits as TaskRunnerTraits;
+use Robo\ResultData;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Yaml\Yaml;
 
@@ -40,7 +41,6 @@ class TestsCommands extends AbstractCommands implements FilesystemAwareInterface
      */
     public function toolkitPhpcs()
     {
-        $tasks = [];
         $grumphpFile = './grumphp.yml.dist';
         $containsQaConventions = false;
 
@@ -54,16 +54,27 @@ class TestsCommands extends AbstractCommands implements FilesystemAwareInterface
                 }
             }
         }
+
+        $composerFile = './composer.json';
+        if (file_exists($composerFile)) {
+            $composerArray = json_decode(file_get_contents($composerFile), true);
+            if (isset($composerArray['extra']['grumphp']['config-default-path'])) {
+                $configDefaultPath = $composerArray['extra']['grumphp']['config-default-path'];
+                $this->say('You should remove the following from your composer.json extra array:');
+                echo "\n\"grumphp\": {\n    \"config-default-path\": \"$configDefaultPath\"\n}\n\n";
+            }
+        }
         
         if ($containsQaConventions) {
-            $tasks[] = $this->taskExec('./vendor/bin/grumphp run');
+            $this->taskExec('./vendor/bin/grumphp run')->run();
         } else {
             $this->say('All Drupal projects in the ec-europa namespace need to use Quality Assurance provided standards.');
             $this->say('Your configuration has to import the resource vendor/ec-europa/qa-automation/dist/qa-conventions.yml.');
             $this->say('Add the following lines to your grumphp.yml.dist:');
             echo "\nimports:\n  - { resource: vendor/ec-europa/qa-automation/dist/qa-conventions.yml }\n\n";
+            return new ResultData(1);
         }
-
+        
         return $this->collectionBuilder()->addTaskList($tasks);
     }
 
