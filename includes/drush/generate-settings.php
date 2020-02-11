d357de709e35ad4bf84b5505253b315526be0dc9
Alex Verbruggen <verbruggenalex@gmail.com>
2020-02-11 13:02:15 +0000

    MULTISITE-23052: First draft of translations folder variable.

--- includes/drush/generate-settings.php
+++ includes/drush/generate-settings.php
@@ -34,7 +34,8 @@ $variables = array(
   'file_temporary_path' => '%%file_temporary_path%%',
   'stage_file_proxy_origin' => '%%stage_file_proxy_origin%%',
   'stage_file_proxy_origin_dir' => '%%stage_file_proxy_origin_dir%%',
-  'stage_file_proxy_hotlink' => '%%stage_file_proxy_hotlink%%'
+  'stage_file_proxy_hotlink' => '%%stage_file_proxy_hotlink%%',
+  'l10n_update_download_store' => '%%l10n_update_download_store%%'
 );
 // Setup individual development variables.
 foreach ($variables as $key => $value) {
