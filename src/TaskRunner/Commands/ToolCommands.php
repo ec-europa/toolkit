509ba978983d09f7c65d60befa5ca91aaff05e19
Alex Verbruggen <verbruggenalex@gmail.com>
2020-02-21 07:49:47 +0000

    MULTISITE-22612: First working draft.

--- src/TaskRunner/Commands/ToolCommands.php
+++ src/TaskRunner/Commands/ToolCommands.php
@@ -49,25 +49,91 @@ class ToolCommands extends AbstractCommands
         $endpointUrl = isset($options['endpoint-url']) ? $options['endpoint-url'] : $this->getConfig()->get("toolkit.notifications_endpoint");
 
         if (isset($endpointUrl)) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $endpointUrl);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            $result = curl_exec($ch);
+            $result = $this->getQaEndpointContent($endpointUrl);
+            $data = json_decode($result, true);
+            foreach ($data as $notification) {
+                $this->io()->warning($notification['title'] . PHP_EOL . $notification['notification']);
+            }
+        }//end if
+    }
 
-            // If request did not fail.
-            if ($result !== false) {
-                // Request was ok? check response code.
-                $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                if ($statusCode == 200) {
-                    $data = json_decode($result, true);
-                    foreach ($data as $notification) {
-                        $this->io()->warning($notification['title'] . PHP_EOL . $notification['notification']);
+    /**
+     * Check composer.json for components that are not whitelisted.
+     *
+     * @command toolkit:whitelist-components
+     *
+     * @option endpoint-url The endpoint for the notifications
+     */
+    public function whitelistComponents(array $options = [
+        'endpoint-url' => InputOption::VALUE_OPTIONAL,
+    ])
+    {
+        $endpointUrl = isset($options['endpoint-url']) ? $options['endpoint-url'] : $this->getConfig()->get("toolkit.whitelistings_endpoint");
+        $basicAuth = getenv('QA_API_BASIC_AUTH') !== false ? getenv('QA_API_BASIC_AUTH') : '';
+        $composerJson = file_get_contents('composer.json') ? json_decode(file_get_contents('composer.json')) : false;
+
+        if (isset($endpointUrl) && isset($composerJson->require)) {
+            $result = $this->getQaEndpointContent($endpointUrl, $basicAuth);
+            $data = json_decode($result, true);
+            $modules = array_filter(array_combine(array_column($data, 'name'), $data));
+
+            // Loop over the require section.
+            foreach ($composerJson->require as $name => $version) {
+                // Check if it's a drupal package.
+                // NOTE: Currently only supports drupal pagackages :(.
+                if (substr($name, 0, 7) === 'drupal/') {
+                    $moduleName = str_replace('drupal/', '', $name);
+                    if (!in_array($name, $modules)) {
+                        $this->io()->warning('The package ' . $name . ' has not been approved by QA to be in your require section of composer.json.');
                     }
-                } else {
-                    $this->io()->error(sprintf('Curl request failed with error code %d. Skipping notification fetching.', $statusCode));
                 }
             }
-            curl_close($ch);
         }//end if
+    }
+
+    /**
+     * Curl function to access endpoint with or without authentication.
+     *
+     * @SuppressWarnings(PHPMD.MissingImport)
+     *
+     * @param string $url The QA endpoint url.
+     * @param string $basicAuth The basic auth.
+     *
+     * @return string
+     */
+    public function getQaEndpointContent(string $url, string $basicAuth = ''): string
+    {
+        $content = '';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        if ($basicAuth !== '') {
+            $header = ['Authorization: Basic ' . $basicAuth];
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        }
+        $result = curl_exec($curl);
+
+        if ($result !== false) {
+            $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            switch ($statusCode) {
+                // Upon success set the content to be returned.
+                case 200:
+                    $content = $result;
+                    break;
+                // Upon other status codes.
+                default:
+                    if ($basicAuth === '') {
+                        throw new \Exception(sprintf('Curl request to endpoint "%s" returned a %u.', $url, $statusCode));
+                    }
+                    // If we tried with authentication, retry without.
+                    $content = $this->getQaEndpointContent($url);
+            }
+        }
+        if ($result === false) {
+            throw new \Exception(sprintf('Curl request to endpoint "%s" failed.', $url));
+        }
+        curl_close($curl);
+
+        return $content;
     }
 }
