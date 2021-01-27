5ac9c5cb6b2846968e280107790f49bfd6aca80f
Miguel Martins <amgmartins@hotmail.com>
2021-01-27 13:08:58 +0000

    DQA-232: Remove HOSTNAME.

--- includes/drush/drushrc.php
+++ includes/drush/drushrc.php
@@ -5,8 +5,12 @@
  * Custom overrides.
  */
 
-if (str_ends_with(getenv('HOSTNAME'), '-acc') || 
-    str_ends_with(getenv('HOSTNAME'), '-prod')) {
+// Specify the base_url that should be used when generating links.
+if (isset(getenv('VIRTUAL_HOST'))) {
   $options['l'] = end(explode(",", getenv('VIRTUAL_HOST')));
+}
+
+// Specify your Drupal core base directory.
+if (isset(getenv('DOCUMENT_ROOT'))) {
   $options['r'] = getenv('DOCUMENT_ROOT');
 }
