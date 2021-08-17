f0bebe079d4be016c18ed5c4fa6b1a43bad1ec4d
João Santos <jonhy81@gmail.com>
2021-08-17 07:11:31 +0000

    DQA-0: Include suite as parameter.

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace EcEuropa\Toolkit\Tests;
 
@@ -17,7 +17,7 @@ abstract class AbstractTest extends TestCase
     /**
      * {@inheritdoc}
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         if (!is_dir($this->getSandboxRoot())) {
             mkdir($this->getSandboxRoot());
@@ -40,7 +40,7 @@ abstract class AbstractTest extends TestCase
     protected function assertContainsNotContains($content, array $expected)
     {
         if (!empty($expected['contains'])) {
-            $this->assertContains($this->trimEachLine($expected['contains']), $this->trimEachLine($content));
+            $this->assertContains($this->trimEachLine($expected['contains']), [$this->trimEachLine($content)]);
             $this->assertEquals(substr_count($this->trimEachLine($content), $this->trimEachLine($expected['contains'])), 1, 'String found more than once.');
         }
 
