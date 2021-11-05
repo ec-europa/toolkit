cdec5f560a71321d18de20618a0e02f13ddd87e9
João Santos <jonhy81@gmail.com>
2021-11-05 14:30:56 +0100

    DQA-000: Add double confirmation to insecure check.

--- src/TaskRunner/Commands/ToolCommands.php
+++ src/TaskRunner/Commands/ToolCommands.php
@@ -380,9 +380,9 @@ class ToolCommands extends AbstractCommands
         $collection = $this->collectionBuilder();
         $result = $collection->taskExecStack()
             ->exec('drush pm:security --format=json')
+            ->silent(true)
             ->printOutput(false)
             ->storeState('insecure')
-            ->silent(true)
             ->run()
             ->getMessage();
 
@@ -392,8 +392,16 @@ class ToolCommands extends AbstractCommands
             $insecurePackages = json_decode($result, true);
             if (is_array($insecurePackages)) {
                 foreach ($insecurePackages as $insecurePackage) {
-                    $this->say("Package " . $insecurePackage['name'] . " have a security update, please update to an safe version.");
-                    $this->componentCheckInsecureFailed = true;
+                    $historyTerms = $this->getPackageDetails($insecurePackage['name'], $insecurePackage['version'], '8.x');
+                    $packageInsecureConfirmation = true;
+                    $msg = "Package " . $insecurePackage['name'] . " have a security update, please update to an safe version.";
+
+                    if (!in_array("insecure", $historyTerms['terms'])) {
+                        $packageInsecureConfirmation = false;
+                        $msg = $msg . " (Confirmation failed, ignored)";
+                    }
+                    $this->say($msg);
+                    $this->componentCheckInsecureFailed = $packageInsecureConfirmation;
                 }
             }
         }
@@ -841,5 +849,54 @@ class ToolCommands extends AbstractCommands
         }
 
         return 0;
+    }
+
+    /**
+     * Call release history of d.org to confirm security alert.
+     */
+    public function getPackageDetails($package, $version, $core)
+    {
+        $name = explode("/", $package)[1];
+        $url = 'https://updates.drupal.org/release-history/' . $name . '/' . $core;
+
+        $releaseHistory = $fullReleaseHistory = [];
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $header = [
+            'Content-Type' => 'application/hal+json'
+        ];
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+
+        $result = curl_exec($curl);
+
+        if ($result !== false) {
+            $fullReleaseHistory[$name] = simplexml_load_string($result);
+            $terms = [];
+            foreach ($fullReleaseHistory[$name]->releases as $release) {
+                foreach ($release as $releaseItem) {
+                    $versionTmp = (string) str_replace($core . "-", "", $releaseItem->version);
+
+                    if (!is_null($version) && Semver::satisfies($versionTmp, $version)) {
+                        foreach ($releaseItem->terms as $term) {
+                            foreach ($term as $termItem) {
+                                $terms[] = (string) $termItem->value;
+                            }
+                        }
+
+                        $releaseHistory = [
+                            'name' => $name,
+                            'version' => (string) $releaseItem->versions,
+                            'terms' => $terms,
+                        ];
+                    }
+                }
+            }
+            return $releaseHistory;
+        }
+
+        $this->say("No release history found.");
+        return 1;
     }
 }
