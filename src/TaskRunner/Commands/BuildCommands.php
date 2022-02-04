6af6fb3ebada5904b2cde7e6a29e9cc5fa2b6669
Joao Silva <geral@joaocsilva.com>
2022-02-04 01:55:42 +0100

    DQA-2617: [4.x] Auto configure URI - PHPUnit.

--- src/TaskRunner/Commands/BuildCommands.php
+++ src/TaskRunner/Commands/BuildCommands.php
@@ -110,10 +110,17 @@ class BuildCommands extends AbstractCommands
         $tasks[] = $this->taskWriteToFile($options['dist-root'] . '/manifest.json')->text(
             json_encode(['version' => $tag, 'sha' => $hash], JSON_PRETTY_PRINT)
         );
-        $tasks[] = $this->taskWriteToFile($options['dist-root'] . '/' . $options['root'] . '/VERSION.txt')->text($tag);
+
+        // Do not process the tag version when running tests.
+        global $argv;
+        if (!(!empty($argv[0]) && strpos($argv[0], 'phpunit') !== false)) {
+            $tasks[] = $this->taskWriteToFile($options['dist-root'] . '/' . $options['root'] . '/VERSION.txt')->text($tag);
+        }
 
         // Copy drush.yml file.
-        $tk_drush = __DIR__ . '/../../../resources/Drush/drush.yml.dist';
+        $tk_drush = file_exists('resources/Drush/drush.yml.dist')
+        ? 'resources/Drush/drush.yml.dist'
+        : 'vendor/ec-europa/toolkit/resources/Drush/drush.yml.dist';
         if (file_exists($tk_drush)) {
             $tasks[] = $this->taskFilesystemStack()
                 ->copy($tk_drush, $options['dist-root'] . '/web/sites/all/drush/drush.yml');
@@ -180,7 +187,9 @@ class BuildCommands extends AbstractCommands
         }
 
         // Copy drush.yml file.
-        $tk_drush = __DIR__ . '/../../../resources/Drush/drush.yml.dist';
+        $tk_drush = file_exists('resources/Drush/drush.yml.dist')
+        ? 'resources/Drush/drush.yml.dist'
+        : 'vendor/ec-europa/toolkit/resources/Drush/drush.yml.dist';
         if (file_exists($tk_drush)) {
             $tasks[] = $this->taskFilesystemStack()
                 ->copy($tk_drush, "$root/sites/all/drush/drush.yml");
@@ -208,15 +217,20 @@ class BuildCommands extends AbstractCommands
      * @command toolkit:build-dev-reset
      *
      * @option root Drupal root.
+     * @option yes  Skip the question.
      */
     public function buildDevReset(array $options = [
         'root' => InputOption::VALUE_REQUIRED,
+        'yes' => InputOption::VALUE_OPTIONAL,
     ])
     {
         $tasks = [];
-
+        $answer = true;
         $question = 'Are you sure you want to proceed? This action cleans up your git repository of any tracked AND untracked files AND folders!';
-        if ($this->confirm($question, false)) {
+        if (!$options['yes']) {
+            $answer = $this->confirm($question, false);
+        }
+        if ($answer) {
             // Clean git.
             $tasks[] = $this->taskGitStack()
                 ->stopOnFail()
@@ -268,7 +282,7 @@ class BuildCommands extends AbstractCommands
      *
      * @command toolkit:build-assets
      *
-     * @option default-theme theme where to build asstes.
+     * @option default-theme theme where to build assets.
      *
      * @option validate or validate=fix to check or fix scss files.
      *
@@ -340,7 +354,7 @@ class BuildCommands extends AbstractCommands
 
                 // Build task collection.
                 $collection = $this->collectionBuilder();
-    
+
                 if (empty($finder->hasResults())) {
                     $collection->taskExecStack()
                         ->exec('cp vendor/ec-europa/toolkit/src/ThemeTaskRunnerConfig/'. $taskRunnerConfigFile . ' ' . $theme_dir . '/' . $taskRunnerConfigFile)
@@ -353,7 +367,7 @@ class BuildCommands extends AbstractCommands
                     ->exec('npm install ' . $options['build-npm-packages'] . ' --save-dev')
                     ->exec('./node_modules/.bin/' . $options['theme-task-runner'])
                     ->stopOnFail();
-    
+
                 // Run and return task collection.
                 return $collection->run();
             }
