4d94db0736c5510f88c03ce38232589b017c8884
João Costa Silva <1574795+joaocsilva@users.noreply.github.com>
2022-10-25 15:58:03 +0200

    DQA-0: Update toolkit version to 8.6.22

--- src/Toolkit.php
+++ src/Toolkit.php
@@ -12,7 +12,7 @@ final class Toolkit
     /**
      * Constant holding the current version.
      */
-    public const VERSION = '8.6.17';
+    public const VERSION = '8.6.22';
 
     /**
      * Returns the QA base url.
