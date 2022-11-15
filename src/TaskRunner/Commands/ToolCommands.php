b4122c7a21b6d344dc450364cfbe6f07f7d9c42e
Joao Silva <geral@joaocsilva.com>
2022-11-15 16:43:03 +0100

    DQA-5323: Move preg_split outside foreach.

--- src/TaskRunner/Commands/ToolCommands.php
+++ src/TaskRunner/Commands/ToolCommands.php
@@ -115,9 +115,9 @@ class ToolCommands extends AbstractCommands
             foreach ($parseOptsFile['upgrade_commands'] as $key => $commands) {
                 foreach ($commands as $command) {
                     $command = str_replace('\\', '', $command);
+                    $parsedCommand = preg_split("/[\s;&|]/", $command, 0, PREG_SPLIT_NO_EMPTY);
                     foreach ($forbiddenCommands as $forbiddenCommand) {
                         if ($key == 'default') {
-                            $parsedCommand = preg_split("/[\s;&|]/", $command, 0, PREG_SPLIT_NO_EMPTY);
                             if (in_array($forbiddenCommand, $parsedCommand)) {
                                 $this->say("The command '$command' is not allowed. Please remove it from 'upgrade_commands' section.");
                                 $reviewOk = false;
