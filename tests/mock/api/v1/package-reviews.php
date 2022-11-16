10d3adc4f92e0e387a9a4623b2b52bc0feca0f99
Joao Silva <geral@joaocsilva.com>
2022-11-16 15:13:20 +0100

    DQA-5483: Rename package-review mock files.

--- tests/mock/api/v1/package-reviews.php
+++ tests/mock/api/v1/package-reviews.php
@@ -2,8 +2,8 @@
 
 header('Content-Type: application/json; charset=utf-8');
 
-if (!empty($_GET['version'])) {
-    require_once $_GET['version'] . '.php';
+if (!empty($_GET['version']) && file_exists('package-reviews-' . $_GET['version'] . '.php')) {
+    require_once 'package-reviews-' . $_GET['version'] . '.php';
 } else {
-    require_once 'all.php';
+    require_once 'package-reviews-all.php';
 }
