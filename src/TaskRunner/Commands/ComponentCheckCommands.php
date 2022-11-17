675250d1f9aa57cd6c17d0556b66f49a891ec417
Joao Silva <geral@joaocsilva.com>
2022-11-17 11:55:16 +0100

    DQA-5482: Align.

--- src/TaskRunner/Commands/ComponentCheckCommands.php
+++ src/TaskRunner/Commands/ComponentCheckCommands.php
@@ -530,8 +530,10 @@ class ComponentCheckCommands extends AbstractCommands
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
         $header = ['Content-Type' => 'application/hal+json'];
         curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+
         $result = curl_exec($curl);
 
         if ($result !== false) {
