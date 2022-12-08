530fdd5c0078faf1037d2fe3ff0feaebb7890d78
Cristiano Gomes <crgomes@gmail.com>
2022-12-08 12:08:32 +0000

    DQA-5473: Tests for DrupalCommands. (#607)

--- tests/Features/Commands/DrupalCommandsTest.php
+++ tests/Features/Commands/DrupalCommandsTest.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 namespace EcEuropa\Toolkit\Tests\Features\Commands {
 
     use EcEuropa\Toolkit\TaskRunner\Commands\DrupalCommands;
-    use EcEuropa\Toolkit\TaskRunner\Runner;
     use EcEuropa\Toolkit\Tests\AbstractTest;
-    use Symfony\Component\Console\Input\StringInput;
-    use Symfony\Component\Console\Output\BufferedOutput;
     use Symfony\Component\Yaml\Yaml;
 
     /**
@@ -31,49 +28,83 @@ namespace EcEuropa\Toolkit\Tests\Features\Commands {
         }
 
         /**
-         * Test Toolkit very own "drupal:settings-setup" command.
+         * Data provider for testDrupalSettingsSetup.
          *
+         * @return array
+         *   An array of test data arrays with assertions.
+         */
+        public function dataProviderSettings()
+        {
+            return $this->getFixtureContent('commands/drupal-settings-setup.yml');
+        }
+
+        /**
+         * Test Toolkit drupal commands.
+         *
+         * @param string $command
+         *   A command.
          * @param array $config
          *   A configuration array.
-         * @param mixed $initial_default_settings
-         *   A initial default settings.
-         * @param mixed $initial_settings
-         *   A initial settings.
-         * @param array $expected
+         * @param string|null $tokens
+         *   Tokens to set in the commit message.
+         * @param array $resources
+         *   Resources needed for the test.
+         * @param array $expectations
          *   Test assertions.
          *
          * @dataProvider dataProvider
          */
-        public function testDrupalSettingsSetup(array $config, $initial_default_settings, $initial_settings, array $expected)
+        public function testDrupalCommands(string $command, array $config = [], string $tokens = null, array $resources = [], array $expectations = [])
         {
             // Setup configuration file.
             if (!empty($config)) {
                 $this->fs->dumpFile($this->getSandboxFilepath('runner.yml'), Yaml::dump($config));
             }
 
-            // Setup test directory.
-            $root = $config['drupal']['root'] ?? 'web';
-            $sites_subdir = $config['drupal']['site']['sites_subdir'] ?? 'default';
-            $settings_root = $this->getSandboxRoot() . '/' . $root . '/sites/' . $sites_subdir;
-            mkdir($settings_root, 0777, true);
-
-            // Setup initial default.settings.php and settings.php, if any.
-            file_put_contents($settings_root . '/default.settings.php', $initial_default_settings);
-
-            // Setup settings.php file, if test case requires it.
-            if ($initial_settings) {
-                file_put_contents($settings_root . '/settings.php', $initial_settings);
+            if ($tokens !== null) {
+                putenv('CI_COMMIT_MESSAGE="' . $tokens . '"');
             }
 
+            $this->prepareResources($resources);
+
             // Run command.
-            $input = new StringInput('drupal:settings-setup --working-dir=' . $this->getSandboxRoot());
-            $runner = new Runner($this->getClassLoader(), $input, new BufferedOutput());
-            $runner->run();
+            $result = $this->runCommand($command);
 
             // Assert expectations.
-            foreach ($expected as $row) {
-                $content = file_get_contents($this->getSandboxFilepath($row['file']));
-                $this->assertDynamic($content, $row);
+            foreach ($expectations as $expectation) {
+                $this->assertDynamic($result['output'], $expectation);
+            }
+        }
+
+        /**
+         * Test Toolkit very own "drupal:settings-setup" command.
+         *
+         * @param string $command
+         * @param array $config
+         *   A configuration array.
+         * @param array $resources
+         *   Resources needed for the test.
+         * @param array $expectations
+         *   Test assertions.
+         *
+         * @dataProvider dataProviderSettings
+         */
+        public function testDrupalSettingsSetupCommands(string $command, array $config, array $resources = [], array $expectations = [])
+        {
+            // Setup configuration file.
+            if (!empty($config)) {
+                $this->fs->dumpFile($this->getSandboxFilepath('runner.yml'), Yaml::dump($config));
+            }
+
+            $this->prepareResources($resources);
+
+            // Run command.
+            $this->runCommand($command, false);
+
+            // Assert expectations.
+            foreach ($expectations as $expectation) {
+                $content = file_get_contents($this->getSandboxFilepath($expectation['file']));
+                $this->assertDynamic($content, $expectation);
             }
         }
 
