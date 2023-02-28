66be1b1ffddd85fe23fa3991a8dde92893b1ede1
Cristiano Gomes <crgomes@gmail.com>
2023-02-28 15:56:05 +0000

    DQA-6261: Include support for key_auth in toolkit.

---
+++ src/AuthorizationBasic.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EcEuropa\Toolkit;
+
+/**
+ * Authorization Basic for QA api.
+ */
+class AuthorizationBasic implements AuthorizationInterface
+{
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function getAuthorizationHeader(): string
+    {
+        return 'Authorization: Basic ' . $this->value;
+    }
+
+}
