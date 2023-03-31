686c5bc4257b98259b0c63c3c43be69a760c0a8c
Joao Silva <geral@joaocsilva.com>
2023-03-31 16:32:50 +0200

    DQA-0: Fix version check to allow version without dot

--- tests/mock/api/v1/toolkit-requirements.php
+++ tests/mock/api/v1/toolkit-requirements.php
@@ -30,7 +30,7 @@ echo '
         "php_version": "7.4"
     },
     "php_version": "7.4",
-    "toolkit": "^3.6.6|^8.6.17|^9.0",
+    "toolkit": "^3.6.6|^8.6.17|^9.0|^10",
     "drupal": "^7.91|^9.3.22|^9.4.7",
     "vendor_list": [
         "drupal",
