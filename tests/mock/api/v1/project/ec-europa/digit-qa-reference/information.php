c3da395275f78212f1e7c54d0862c6d8f7052783
João Costa Silva <1574795+joaocsilva@users.noreply.github.com>
2023-04-03 11:00:50 +0200

    DQA-0: Fix version check to allow version without dot (#657)

--- tests/mock/api/v1/project/ec-europa/digit-qa-reference/information.php
+++ tests/mock/api/v1/project/ec-europa/digit-qa-reference/information.php
@@ -7,18 +7,18 @@ echo '
     {
         "nid": "2075",
         "name": "digit-qa-reference",
-        "acceptance": "https://webgate.acceptance.ec.europa.eu/d/DIGIT/digit-qa",
+        "acceptance": "https://webgate.acceptance.ec.europa.eu/fpfis/qa",
         "critical": "No",
         "introduction": "[DIGIT] Internal website for Quality Assurance endpoints.",
-        "core": "9.4.8",
-        "core_acc": "9.4.8",
+        "core": "10.0.5",
+        "core_acc": "10.0.7",
         "profile_acceptance": "minimal",
         "full_name": "ec-europa/digit-qa-reference",
         "screenshot": "/fpfis/qa/sites/default/files/screenshots/digit-qa-reference.png",
         "dg_agency": "DIGIT",
         "type": "Drupal 9",
         "homepage": "https://webgate.ec.europa.eu/fpfis/qa",
-        "production_version": "4.5.2",
+        "production_version": "6.1.0",
         "profile": "minimal",
         "status": "Live",
         "repository": "https://github.com/ec-europa/digit-qa-reference",
@@ -36,12 +36,12 @@ echo '
         "gitlab_topics": "location:dc",
         "gitlab_variables": "CI_DEPLOY_BRANCH=master,DIFFY_PROJECT_ID=,CI_DEMO_ENV_SCOPE=shared,TOOLKIT_PROJECT_ID=digit-qa,NO_CICD=True",
         "weight": "0",
-        "apache_version": "2.4.41-4ubuntu3.12",
+        "apache_version": "2.4.41-4ubuntu3.14",
         "cluster": "FPFIS",
-        "housing_type": "Shared",
+        "housing_type": "Dedicated",
         "gitlab_timeout": "",
         "housing": "DC",
-        "php_version": "8.1.12",
+        "php_version": "8.1.16",
         "gitlab_id": "1788",
         "shared_with": [
             {
@@ -63,7 +63,8 @@ echo '
                     }
                 ]
             }
-        ]
+        ],
+        "availability": ""
     }
 ]
 ';
