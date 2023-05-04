99a3246612f308cbc169d12ad569c6a0d055aea9
Joao Silva <geral@joaocsilva.com>
2023-05-04 11:07:31 +0200

    Release 10.0.0-beta7

--- src/Toolkit.php
+++ src/Toolkit.php
@@ -14,7 +14,7 @@ final class Toolkit
     /**
      * Constant holding the current version.
      */
-    public const VERSION = '10.0.0-beta6';
+    public const VERSION = '10.0.0-beta7';
 
     /**
      * Returns the Toolkit root.
