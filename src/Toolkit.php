da579e84f8333250590f93225db6dc5f794fae05
Joao Silva <geral@joaocsilva.com>
2023-06-30 12:03:47 +0200

    DQA-7313: Release 9.11.0

--- src/Toolkit.php
+++ src/Toolkit.php
@@ -14,7 +14,7 @@ final class Toolkit
     /**
      * Constant holding the current version.
      */
-    public const VERSION = '9.10.0';
+    public const VERSION = '9.11.0';
 
     /**
      * Returns the Toolkit root.
