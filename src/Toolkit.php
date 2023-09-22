425c92e1d3509a6834ebb9d3a784a163a6167925
Cristiano Gomes <crgomes@gmail.com>
2023-09-22 11:43:07 +0100

    DQA-7826: Add Toolkit version number

--- src/Toolkit.php
+++ src/Toolkit.php
@@ -12,7 +12,7 @@ final class Toolkit
     /**
      * Constant holding the current version.
      */
-    public const VERSION = '10.3.0';
+    public const VERSION = '10.3.1';
 
     /**
      * Returns the Toolkit root.
