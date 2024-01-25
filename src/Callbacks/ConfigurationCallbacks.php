1bb31e616e9fefa3ac4a346a3abb3ac7f281b866
João Costa Silva <1574795+joaocsilva@users.noreply.github.com>
2024-01-25 16:21:41 +0100

    DQA-8117: Detect PHPStan includes if using phpstan/extension-installer (#726)

---
+++ src/Callbacks/ConfigurationCallbacks.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EcEuropa\Toolkit\Callbacks;
+
+use EcEuropa\Toolkit\TaskRunner\Commands\ToolCommands;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class containing configuration check callbacks.
+ */
+class ConfigurationCallbacks
+{
+
+    /**
+     * If grumphp package is not present in a project, then grumphp config file must not be present.
+     */
+    public static function grumpPhpIsNotInstalled(): bool
+    {
+        return !ToolCommands::getPackagePropertyFromComposer('phpro/grumphp');
+    }
+
+    /**
+     * If project is using phpstan/extension-installer then should not manually include extensions.
+     */
+    public static function validatePhpStan(): bool
+    {
+        $file = 'phpstan.neon';
+        // Stop if the config file do not exist or the package is not installed.
+        if (!file_exists($file) || !ToolCommands::getPackagePropertyFromComposer('phpstan/extension-installer')) {
+            return true;
+        }
+        // Load the config file and check for the includes.
+        $config = Yaml::parseFile($file);
+        return empty($config['includes']);
+    }
+
+}
