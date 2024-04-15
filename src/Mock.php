6adb50b4891e0ef675c1eff35e7bf512b540007c
Michal Kozlowski <mr.m.kozlowski@gmail.com>
2024-04-15 16:35:13 +0200

    DQA-9080: Adapt MOCK "toolkit-requirements" endpoint to return forbidden environment variables.

--- src/Mock.php
+++ src/Mock.php
@@ -17,7 +17,7 @@ final class Mock
      *
      * @var string
      */
-    private static string $defaultTag = '0.0.4';
+    private static string $defaultTag = '0.0.5';
 
     /**
      * The directory to download the mock to.
