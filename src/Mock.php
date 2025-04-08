749bd0fae09df6e3ea61e83a6db9a375c92e6bd5
João Costa Silva <geral@joaocsilva.com>
2025-04-08 10:03:20 +0200

    DQA-11423: Prepare release 10.19.1

--- src/Mock.php
+++ src/Mock.php
@@ -17,7 +17,7 @@ final class Mock
      *
      * @var string
      */
-    private static string $defaultTag = '0.0.39';
+    private static string $defaultTag = '0.0.41';
 
     /**
      * The directory to download the mock to.
