2750a6a8426bc34a3156c63b39712e99ef36a474
João Costa Silva <1574795+joaocsilva@users.noreply.github.com>
2025-06-17 11:09:24 +0200

    DQA-11776: Update image registry (#812)

--- src/Mock.php
+++ src/Mock.php
@@ -17,7 +17,7 @@ final class Mock
      *
      * @var string
      */
-    private static string $defaultTag = '0.0.48';
+    private static string $defaultTag = '0.0.51';
 
     /**
      * The directory to download the mock to.
