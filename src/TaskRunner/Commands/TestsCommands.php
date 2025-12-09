66a874d52591ab69259624d31705573e9580984a
João Costa Silva <geral@joaocsilva.com>
2025-12-09 10:14:13 +0100

    DQA-13046: Debug parallel

--- src/TaskRunner/Commands/TestsCommands.php
+++ src/TaskRunner/Commands/TestsCommands.php
@@ -645,6 +645,7 @@ class TestsCommands extends AbstractCommands
             if ($options['printer'] === true) {
                 $opts .= " --printer=" . $this->getConfig()->get('toolkit.test.phpunit.printer');
             }
+            $this->writeln('Matches: ' . implode(', ', $matches[1]));
             $suites = array_map('trim', $matches[1]);
             $this->writeln('Suites found: ' . implode(', ', $suites));
             foreach ($suites as $suite) {
