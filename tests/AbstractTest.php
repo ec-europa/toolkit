f04d64b137056305f517f484e1c82b7df209f692
João Costa Silva <geral@joaocsilva.com>
2025-12-19 14:52:35 +0100

    DQA-13152: Compatibility with phpunit9.5

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -6,7 +6,6 @@ namespace EcEuropa\Toolkit\Tests;
 
 use EcEuropa\Toolkit\TaskRunner\Runner;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\TestStatus\Unknown;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\NullOutput;
