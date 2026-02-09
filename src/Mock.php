af333dea8d3b9e9cf708cda56f8d5b2a1581ab55
João Costa Silva <geral@joaocsilva.com>
2026-02-09 16:35:57 +0100

    DROPSEC-16324: Automatically update tests files with mock version

--- src/Mock.php
+++ src/Mock.php
@@ -17,7 +17,7 @@ final class Mock
      *
      * @var string
      */
-    private static string $defaultTag = '0.0.84';
+    private static string $defaultTag = '0.0.85';
 
     /**
      * The directory to download the mock to.
