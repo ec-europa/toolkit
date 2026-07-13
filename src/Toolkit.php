085fc2077b712203100b01b08f3602f80d45e92f
João Costa Silva <geral@joaocsilva.com>
2026-07-13 17:00:07 +0200

    DQA-14497: Prepare release 10.29.3

--- src/Toolkit.php
+++ src/Toolkit.php
@@ -14,7 +14,7 @@ final class Toolkit
     /**
      * Constant holding the current version.
      */
-    public const VERSION = '10.29.2';
+    public const VERSION = '10.29.3';
 
     /**
      * The Toolkit repository.
